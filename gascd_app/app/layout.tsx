a9fa7ace06d1db862c0ef01471e8efca8ee7dcfa
Sam <73955537+samfallowfield@users.noreply.github.com>
2025-01-13 10:14:48 +0000

    Axe Setup (#12)
    * set up Axe Config
    
    * working version of Axe with ignore rule set up
    
    * moved axe.ts to utils folder, tidied up

--- gascd_app/app/layout.tsx
+++ gascd_app/app/layout.tsx
@@ -1,18 +1,21 @@
-import '../src/styles/globals.scss'
+import '../src/styles/globals.scss';
+import Axe from '../src/utils/axe';
 
 export const metadata = {
   title: 'DHSC: Get Adult Social Care Data',
-  description: 'Department of Health and Social Care: Get Adult Social Care Data Platform',
-}
+  description:
+    'Department of Health and Social Care: Get Adult Social Care Data Platform',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Axe />
       <body className="govuk-frontend-supported">{children}</body>
     </html>
-  )
+  );
 }
---
+++ gascd_app/src/utils/axe.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+
+const Axe: React.FC = () => {
+  if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+    Promise.all([import('@axe-core/react'), import('react-dom')]).then(
+      ([Axe, ReactDOM]) => {
+        Axe.default(React, ReactDOM, 1000, {
+          rules: [
+            {
+              id: 'region',
+              enabled: true,
+              selector: '*:not(.govuk-phase-banner)',
+            },
+          ],
+        });
+      }
+    );
+  }
+  return null;
+};
+
+export default Axe;
