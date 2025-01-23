fc623e257387fb55d378ef88b7a2c299dbc485b3
sam brown GASCD Git <sam.brown@madetech.com>
2025-01-23 11:03:23 +0000

    update login page

--- gascd_app/app/login/page.tsx
+++ gascd_app/app/login/page.tsx
@@ -1,24 +1,24 @@
-'use client'
+'use client';
 
-import React, { useCallback, useState } from "react";
-import Layout from "../../src/components/common/layout/Layout";
-import StandardButton from "../../src/components/common/buttons/functionality/standard-button/StandardButton";
-import Link from "next/link";
-import {signIn, signOut, useSession} from "next-auth/react";
+import React, { useCallback, useState } from 'react';
+import Layout from '../../src/components/common/layout/Layout';
+import StandardButton from '../../src/components/common/buttons/functionality/standard-button/StandardButton';
+import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
   // const handleSubmit = useCallback(() => {
   //   sessionStorage.setItem("isLoggedIn", "true");
   //   sessionStorage.setItem("email", email);
   // }, [email]);
 
   const handleSubmit = () => {
-    signIn("azure-ad-b2c", { callbackUrl: "/home" })
+    signIn('azure-ad-b2c', { callbackUrl: '/home' });
   };
 
   return (
-    <Layout showLoginInformation={false}>
+    <Layout showLoginInformation={false} currentPage="login">
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-two-thirds">
           <h1 className="govuk-heading-l">Sign In</h1>
