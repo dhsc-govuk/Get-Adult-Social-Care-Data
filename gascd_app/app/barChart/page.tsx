1bbee3ee59cb3ea79c244ae9893655b0c83aaa6d
sam fallowfield <sam.fallowfield@madetech.com>
2025-01-31 14:44:56 +0000

    initial bar chart

---
+++ gascd_app/app/barChart/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import GraphTest from '@/components/graphs/BarChart';
+import React from 'react';
+
+const barChartPage: React.FC = () => {
+  return (
+    <main>
+      <GraphTest />
+    </main>
+  );
+};
+
+export default barChartPage;
