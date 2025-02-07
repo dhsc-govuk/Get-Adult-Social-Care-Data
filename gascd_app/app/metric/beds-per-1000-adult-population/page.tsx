42569aee35ae8cc64c2d1b1a8d398c9f5bebfe80
sam brown GASCD Git <sam.brown@madetech.com>
2025-02-07 11:51:29 +0000

    data being displayed in page

--- gascd_app/app/metric/beds-per-1000-adult-population/page.tsx
+++ gascd_app/app/metric/beds-per-1000-adult-population/page.tsx
@@ -11,17 +11,16 @@ import { Indicator } from '@/data/interfaces/Indicator';
 
 const BedsPer1000AdultPopulationPage: React.FC = () => {
   const [indicatorService, setIndicatorService] =
-    useState<IndicatorService| null>(null);
+    useState<IndicatorService | null>(null);
 
   const [metricView, setMetricView] = useState('barchart');
 
   useEffect(() => {
     const fetchData = async () => {
-      const data: Indicator[] = await IndicatorFetchService.getData('')
-      const displayData: IndicatorDisplay = await IndicatorFetchService.getDisplayData('');
-      setIndicatorService(
-        new IndicatorService(data, displayData)
-      );
+      const data: Indicator[] = await IndicatorFetchService.getData('');
+      const displayData: IndicatorDisplay =
+        await IndicatorFetchService.getDisplayData('');
+      setIndicatorService(new IndicatorService(data, displayData));
     };
     fetchData();
   }, []);
@@ -30,8 +29,7 @@ const BedsPer1000AdultPopulationPage: React.FC = () => {
 
   useEffect(() => {
     if (svgContainerRef.current && indicatorService) {
-      const barchart =
-        indicatorService.createBarchart()
+      const barchart = indicatorService.createBarchart();
 
       svgContainerRef.current.innerHTML = '';
       if (barchart) {
@@ -51,11 +49,13 @@ const BedsPer1000AdultPopulationPage: React.FC = () => {
       showLoginInformation={true}
       currentPage="beds-per-1000-adult-population"
     >
-      <a href="#" className="govuk-back-link">Back</a>
+      <a href="#" className="govuk-back-link">
+        Back
+      </a>
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-one-third">
           <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full">              
+            <div className="govuk-grid-column-full">
               <ContentSidePanel />
             </div>
           </div>
@@ -71,25 +71,28 @@ const BedsPer1000AdultPopulationPage: React.FC = () => {
 
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-full">
-              <h2 className="govuk-heading-m">Indicator definition and supporting information</h2>
-              <p className="govuk-body">The total number of beds recorded by care providers across 
-                health and adult social care, adjusted to a rate of 100,000 adults 
-                in the local authority population.</p>     
+              <h2 className="govuk-heading-m">
+                Indicator definition and supporting information
+              </h2>
               <p className="govuk-body">
-                'For detailed information about this indicator, including data 
-                definitions, data source, update schedule and limitations to be 
+                The total number of beds recorded by care providers across
+                health and adult social care, adjusted to a rate of 100,000
+                adults in the local authority population.
+              </p>
+              <p className="govuk-body">
+                For detailed information about this indicator, including data
+                definitions, data source, update schedule and limitations to be
                 aware of being using this data, go to&nbsp;
-                  <a href="#" className="govuk-link">
-                    supporting information for this data
-                  </a>.
+                <a href="#" className="govuk-link">
+                  supporting information for this data
+                </a>
+                .
               </p>
             </div>
           </div>
           <div className="govuk-grid-row govuk-!-margin-bottom-2">
             <div className="govuk-grid-column-full">
-              <p className="govuk-body">
-                
-              </p>
+              <p className="govuk-body"></p>
             </div>
           </div>
           <div className="govuk-grid-row">
@@ -107,6 +110,5 @@ const BedsPer1000AdultPopulationPage: React.FC = () => {
     </Layout>
   );
 };
-
 
 export default BedsPer1000AdultPopulationPage;
