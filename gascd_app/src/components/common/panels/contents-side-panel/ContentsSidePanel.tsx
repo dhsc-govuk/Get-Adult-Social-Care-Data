33ef43324b9e6d3faf6fb8a37a282a21fd95a444
Ashley Perrett <ashley.perrett@madetech.com>
2025-02-07 10:26:54 +0000

    add initial indicator page layout

---
+++ gascd_app/src/components/common/panels/contents-side-panel/ContentsSidePanel.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const DataCategoriesSidePanel: React.FC = () => {
+  const dataCategories: Array<string> = [
+    "Indicator definition and supporting information",
+    "Chart",
+    "Table"
+  ];
+  return (
+    <div className="dhsc-blue-panel-container">
+      <h4 className="govuk-heading-m govuk-!-margin-bottom-5 dhsc-!-text-white">Contents</h4>
+      <ul className="govuk-list govuk-!-margin-left-3">
+        {dataCategories.map((contents, index) => (
+          <li key={index} className="govuk-!-margin-bottom-3 dhsc-!-text-white">
+            <a
+              className="govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+              href="#"
+            >
+              {contents}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+
+
+
+  );
+};
+
+export default DataCategoriesSidePanel;
