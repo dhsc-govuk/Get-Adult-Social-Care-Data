fe43fa1a554aee1071cb25530816e5a6dc88a57a
sam brown GASCD Git <sam.brown@madetech.com>
2025-02-26 17:01:45 +0000

    pull through smart insights

--- gascd_app/app/metric/total-beds/page.tsx
+++ gascd_app/app/metric/total-beds/page.tsx
@@ -12,12 +12,15 @@ import { useState, useEffect, useRef } from 'react';
 import { TotalBedsFilters } from '@/data/interfaces/TotalBedsFilters';
 import { useSearchParams } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+import SmartInsightsFetchService from '@/services/smart-insights/SmartInsightsFetchService';
+import { parseMarkdownBlocks } from '@/utils/parseMarkdown';
 
 const TotalBedsPage: React.FC = () => {
   const [indicatorService, setIndicatorService] =
     useState<IndicatorService | null>(null);
 
   const { data: session, status } = useSession();
+  const [smartInsights, setSmartInsights] = useState<string[]>(['']);
 
   const [locationId, setlocationId] = useState<string>();
   const [locationType, setlocationType] = useState<string>();
@@ -67,6 +70,10 @@ const TotalBedsPage: React.FC = () => {
         const displayData: IndicatorDisplay =
           await IndicatorFetchService.getDisplayData('');
         setIndicatorService(new IndicatorService(data, displayData));
+        const insights: string[] =
+          await SmartInsightsFetchService.getData(indicatorQuery);
+        setSmartInsights(insights);
+        console.log('Updated Smart Insights:', smartInsights);
       };
       fetchData();
     }
@@ -204,83 +211,7 @@ const TotalBedsPage: React.FC = () => {
             selectedFilters={parsedFilters}
           />
 
-          <h2 id="smart-insights" className="govuk-heading-m">
-            Smart insights (experimental)
-          </h2>
-          <div className="govuk-inset-text">
-            Smart insights use artificial intelligence (AI) to analyse this
-            data, highlighting trends and patterns to support your own analysis.
-            As this is experimental, verify the insights to check they are
-            appropriate and meet your needs.{' '}
-            <a className="govuk-link" href="../0-3/help/smart-insights.html">
-              Learn more about smart insights
-            </a>
-          </div>
-
-          <h3 className="govuk-heading-s">Summary of key trends</h3>
-          <p className="govuk-body">
-            Suffolk has a higher number of dementia residential beds per 100,000
-            adult population (648.8) compared to the East of England region
-            (486.6) and England overall (432.8). This suggests a stronger focus
-            on residential dementia care in Suffolk than in many other areas.
-          </p>
-
-          <p className="govuk-body">
-            Additionally, while Suffolk provides more total care beds per
-            100,000 people (2,398.4) than the East of England region (1,969.5),
-            its provision of dementia nursing beds (160.1 per 100,000) is
-            slightly lower than the national average (182.1). This indicates
-            that while residential dementia care is well-supported, access to
-            specialist nursing care for dementia may be more limited compared to
-            the national picture.
-          </p>
-
-          <h3 className="govuk-heading-s">
-            Insight 1: Strong focus on dementia residential care in Suffolk
-          </h3>
-          <p className="govuk-body">
-            Suffolk has a 33% higher provision of dementia residential beds per
-            100,000 people than the East of England region and 50% more than the
-            national average. This suggests that dementia residential care is
-            more widely available in Suffolk compared to other areas.
-          </p>
-          <h4 className="govuk-heading-s">How you might use this insight</h4>
-
-          <ul className="govuk-list govuk-list--bullet">
-            <li>
-              Review how demand for dementia residential care compares to other
-              types of adult social care to ensure resources align with future
-              needs.
-            </li>
-            <li>
-              Assess whether the focus on dementia residential care affects the
-              availability of other care types, such as general nursing or
-              specialist mental health care.
-            </li>
-          </ul>
-
-          <h3 className="govuk-heading-s">
-            Insight 2: Lower provision of dementia nursing care compared to
-            national average
-          </h3>
-          <p className="govuk-body">
-            While Suffolk has a strong provision of dementia residential care,
-            its dementia nursing care beds per 100,000 adult population (160.1)
-            are lower than the national average (182.1). This may indicate fewer
-            options for those needing more intensive nursing support.
-          </p>
-          <h4 className="govuk-heading-s">How you might use this insight</h4>
-
-          <ul className="govuk-list govuk-list--bullet">
-            <li>
-              Explore whether demand for dementia nursing care in Suffolk is
-              increasing and if additional provision is needed.
-            </li>
-            <li>
-              Consider whether individuals with complex dementia care needs have
-              sufficient access to specialised nursing care locally.
-            </li>
-          </ul>
+          <div>{parseMarkdownBlocks(smartInsights)}</div>
 
           <p className="govuk-body">
             <a href="javascript:history.back()" className="govuk-link">
