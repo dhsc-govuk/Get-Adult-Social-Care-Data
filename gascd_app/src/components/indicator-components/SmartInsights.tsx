9d01f47a08220a63528c109307ffd1be209fe592
sam brown GASCD Git <sam.brown@madetech.com>
2025-03-11 12:20:27 +0000

    add conditional rendering

--- gascd_app/src/components/indicator-components/SmartInsights.tsx
+++ gascd_app/src/components/indicator-components/SmartInsights.tsx
@@ -1,30 +1,29 @@
 'use client';
 
-import { parseMarkdownBlocks } from "@/utils/parseMarkdown"
+import { parseMarkdownBlocks } from '@/utils/parseMarkdown';
+import React from 'react';
 
 type Props = {
-	smartInsights: string[];
-}
+  smartInsights: string[];
+};
 const SmartInsights: React.FC<Props> = ({ smartInsights }) => {
-	return (
-		<div>
-			<h2 id="smart-insights" className="govuk-heading-m">
-				Smart insights (experimental)
-			</h2>
-			<div className="govuk-inset-text">
-				Smart insights use artificial intelligence (AI) to analyse this
-				data, highlighting trends and patterns to support your own
-				analysis. As this is experimental, verify the insights to check
-				they are appropriate and meet your needs.&nbsp;
-				<a href="/help/smart-insights" className="govuk-link">
-					Learn more about smart insights
-				</a>
-			</div>
-			<div>
-				{parseMarkdownBlocks(smartInsights)}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h2 id="smart-insights" className="govuk-heading-m">
+        Smart insights (experimental)
+      </h2>
+      <div className="govuk-inset-text">
+        Smart insights use artificial intelligence (AI) to analyse this data,
+        highlighting trends and patterns to support your own analysis. As this
+        is experimental, verify the insights to check they are appropriate and
+        meet your needs.&nbsp;
+        <a href="/help/smart-insights" className="govuk-link">
+          Learn more about smart insights
+        </a>
+      </div>
+      <div>{parseMarkdownBlocks(smartInsights)}</div>
+    </div>
+  );
 };
 
-export default SmartInsights
+export default SmartInsights;
