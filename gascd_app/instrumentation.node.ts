49584ea06fe0ae22064e6e7a0d4d4407b489c0ef
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-07-09 15:08:44 +0100

    Remove unused console logger

--- gascd_app/instrumentation.node.ts
+++ gascd_app/instrumentation.node.ts
@@ -5,26 +5,6 @@ import {
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { ATTR_SERVICE_NAMESPACE } from '@opentelemetry/semantic-conventions/incubating';
-import { LogRecord, LogRecordProcessor } from '@opentelemetry/sdk-logs';
-import { Context, trace } from '@opentelemetry/api';
-
-class ConsoleLogRecordProcessor implements LogRecordProcessor {
-  forceFlush(): Promise<void> {
-    return Promise.resolve();
-  }
-  shutdown(): Promise<void> {
-    return Promise.resolve();
-  }
-  onEmit(logRecord: LogRecord, context: Context): void {
-    const currentSpan = trace.getSpan(context);
-    const traceId = currentSpan ? currentSpan.spanContext().traceId : 'none';
-    const enrichedAttributes = { ...logRecord.attributes, 'trace.id': traceId };
-    console.log('Log record:', {
-      ...logRecord,
-      attributes: enrichedAttributes,
-    });
-  }
-}
 
 const customResource = resourceFromAttributes({
   [ATTR_SERVICE_NAME]: 'frontend',
@@ -36,7 +16,6 @@ const options: AzureMonitorOpenTelemetryOptions = {
     connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
   },
   resource: customResource,
-  logRecordProcessors: [new ConsoleLogRecordProcessor()],
 };
 
 useAzureMonitor(options);
