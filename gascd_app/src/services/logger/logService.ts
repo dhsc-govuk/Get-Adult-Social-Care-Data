7e201bd1b593086c334536482eb1617c89b7bae4
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-07-10 16:56:13 +0100

    Enable the server logging service in dev mode

--- gascd_app/src/services/logger/logService.ts
+++ gascd_app/src/services/logger/logService.ts
@@ -1,22 +1,19 @@
+// Service to let the browser client log messages back to the server via the api
 class LogService {
+  public static async logEvent(message: string): Promise<void> {
+    const response = await fetch('/api/logger', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    });
 
-    public static async logEvent(message: string): Promise<void>{
-
-        const isLocal = process.env.NEXT_PUBLIC_APP_ENV === 'local';
-        if(isLocal){
-            return;
-        }
-        const response = await fetch('/api/logger', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({message}),
-        });
-    
-        if (!response.ok) {
-            console.error(`Error logging event: ${response.status} ${response.statusText}`);
-        }
+    if (!response.ok) {
+      console.error(
+        `Error logging event: ${response.status} ${response.statusText}`
+      );
+    }
   }
 }
 
