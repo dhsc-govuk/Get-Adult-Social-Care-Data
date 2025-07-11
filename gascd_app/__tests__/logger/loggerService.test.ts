691d8ae6f238a3a0971d3c6ff4d14094bf3e66e5
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-07-11 11:43:45 +0100

    Remove old test - api logger is used in development now as well

--- gascd_app/__tests__/logger/loggerService.test.ts
+++ gascd_app/__tests__/logger/loggerService.test.ts
@@ -9,7 +9,10 @@ describe('logEvent Service', () => {
   });
 
   it('should send a POST request to the logger endpoint with the message', async () => {
-    mockFetch.mockResolvedValueOnce({ ok: true, json: () => Promise.resolve({}) });
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve({}),
+    });
 
     const message = 'Test log message';
     await LogService.logEvent(message);
@@ -25,27 +28,30 @@ describe('logEvent Service', () => {
 
   it('should not throw an error when logging', async () => {
     process.env.NEXT_PUBLIC_APP_ENV = '';
-    const mockResponse = { ok: true, json: () => Promise.resolve({ data: 'test' }) };
+    const mockResponse = {
+      ok: true,
+      json: () => Promise.resolve({ data: 'test' }),
+    };
     mockFetch.mockResolvedValueOnce(mockResponse);
 
-    await expect(LogService.logEvent("test")).resolves.not.toThrow();
+    await expect(LogService.logEvent('test')).resolves.not.toThrow();
   });
 
   it('should log an error to the console if the response is not ok', async () => {
-    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
-    mockFetch.mockResolvedValueOnce({ ok: false, status: 500, statusText: 'Internal Server Error' });
+    const mockConsoleError = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+    mockFetch.mockResolvedValueOnce({
+      ok: false,
+      status: 500,
+      statusText: 'Internal Server Error',
+    });
 
     await LogService.logEvent('Test message');
 
-    expect(mockConsoleError).toHaveBeenCalledWith('Error logging event: 500 Internal Server Error');
+    expect(mockConsoleError).toHaveBeenCalledWith(
+      'Error logging event: 500 Internal Server Error'
+    );
     mockConsoleError.mockRestore();
   });
-
-  it('should not send a POST request when local', async () => {
-    process.env.NEXT_PUBLIC_APP_ENV = 'local';
-    const message = 'Test log message';
-    await LogService.logEvent(message);
-
-    expect(mockFetch).not.toHaveBeenCalled();
-});
 });
