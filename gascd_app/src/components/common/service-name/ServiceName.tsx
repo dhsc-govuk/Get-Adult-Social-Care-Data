fb8241d030e5adabd3ecb79a756ba4998c9631a8
andiempett <andi.empett@madetech.com>
2025-07-14 16:28:50 +0100

    formatting of header site header components

--- gascd_app/src/components/common/service-name/ServiceName.tsx
+++ gascd_app/src/components/common/service-name/ServiceName.tsx
@@ -10,28 +10,28 @@ type Props = {
 const ServiceName: React.FC<Props> = ({ session }) => {
     return (
         <section aria-label="Service information" className="govuk-service-navigation"
-        data-module="govuk-service-navigation">
-        <div className="govuk-width-container">
-        <div className="govuk-service-navigation__container">
-        <span className="govuk-service-navigation__service-name">
-        <Link href="/" className="govuk-service-navigation__link">
-        Get adult social care data
-        </Link>
-        </span>
-        {!session ? (
-            <>&nbsp;</>
-        ) : (
-            <nav aria-label="Menu" className="govuk-service-navigation__wrapper">
-            <ul className="govuk-service-navigation__list" id="navigation">
-            <li className="govuk-service-navigation__item app-sign-out" data-module="edge" 
-                style={{marginLeft: "auto", padding: "10px 0px"}}>
-                <LogoutButton />
-            </li>
-            </ul>
-            </nav>
-        )}    
-        </div>
-        </div>
+            data-module="govuk-service-navigation">
+            <div className="govuk-width-container">
+                <div className="govuk-service-navigation__container">
+                    <span className="govuk-service-navigation__service-name">
+                        <Link href="/" className="govuk-service-navigation__link">
+                            Get adult social care data
+                        </Link>
+                    </span>
+                    {!session ? (
+                        <>&nbsp;</>
+                    ) : (
+                        <nav aria-label="Menu" className="govuk-service-navigation__wrapper">
+                            <ul className="govuk-service-navigation__list" id="navigation">
+                                <li className="govuk-service-navigation__item app-sign-out" data-module="edge"
+                                    style={{ marginLeft: "auto", padding: "10px 0px" }}>
+                                    <LogoutButton />
+                                </li>
+                            </ul>
+                        </nav>
+                    )}
+                </div>
+            </div>
         </section>
     );
 };
