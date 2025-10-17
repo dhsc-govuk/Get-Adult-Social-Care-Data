2ee637e6fc541af35419c3293b115ad323423d1c
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-10-17 16:41:31 +0100

    Switch initial auth and session handling to better auth

--- gascd_app/app/(protected)/layout.tsx
+++ gascd_app/app/(protected)/layout.tsx
@@ -1,15 +1,22 @@
 import { redirect } from 'next/navigation';
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../api/auth/authOptions';
+//import { getServerSession } from 'next-auth/next';
+//import { authOptions } from '../api/auth/authOptions';
 import { verifyAuthToken } from '../../src/helpers/auth/verifyAuthToken';
 import logger from '@/utils/logger';
+import { auth } from '@/utils/auth';
+import { headers } from 'next/headers';
 
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
+  //const session = await getServerSession(authOptions);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   if (process.env.LOCAL_AUTH == 'true') {
     if (session?.user) {
       // Skip jwt validation if doing local auth
@@ -17,15 +24,9 @@ export default async function AuthLayout({
     }
   }
 
-  if (!session || !session.idToken) {
+  if (!session || !session.user) {
     redirect('/login');
   }
 
-  try {
-    await verifyAuthToken(session.idToken);
-  } catch (error) {
-    logger.error('Error verifying token:', error);
-    redirect('/login');
-  }
   return <>{children}</>;
 }
--- gascd_app/src/utils/auth.ts
+++ gascd_app/src/utils/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from 'better-auth';
 import Database from 'better-sqlite3';
+import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
   database: new Database('./sqlite.db'),
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
