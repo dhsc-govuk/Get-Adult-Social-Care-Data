64d0788ea94d532d2a677542cde657fd14917a1e
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-10-20 17:05:19 +0100

    Sync custom properties from B2C on every login

--- gascd_app/src/utils/authPlugins.ts
+++ gascd_app/src/utils/authPlugins.ts
@@ -10,8 +10,10 @@ export const B2CPlugin = () => {
         clientSecret: process.env.AZURE_AD_CLIENT_SECRET as string,
         discoveryUrl: `https://${process.env.AZURE_AD_TENANT_NAME}.b2clogin.com/${process.env.AZURE_AD_TENANT_NAME}.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=${process.env.AZURE_AD_B2C_USER_SIGN_IN}`,
         scopes: ['openid', 'offline_access', 'profile'],
+        // Ensure location details after updated on every login
+        overrideUserInfo: true,
+        // Custom userinfo method, to deal with B2Cs list of emails
         getUserInfo: async (tokens) => {
-          // Custom B2C userinfo method, to deal with B2Cs list of emails
           if (tokens.idToken) {
             const decoded = decodeJwt(tokens.idToken) as {
               sub: string;
@@ -30,8 +32,8 @@ export const B2CPlugin = () => {
           }
           return null;
         },
+        // Map B2C location IDs to our internal fields
         mapProfileToUser: (profile) => {
-          // Map B2C location IDs
           return {
             id: profile.id,
             locationId: profile.extension_Location_ID,
