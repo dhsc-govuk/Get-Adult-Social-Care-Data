365d0fb881422403148bf4cdbbc04ef7a6f59d2b
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-10-20 19:10:08 +0100

    Switch api middleware check to better-auth

--- gascd_app/middleware.ts
+++ gascd_app/middleware.ts
@@ -1,17 +1,19 @@
-// Middleware to protect /api endpoints
-// (views are protected by AuthLayout)
-import { withAuth } from 'next-auth/middleware';
-export default withAuth(async function middleware(req) {}, {
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized: ({ token }) => !!token,
-  },
-});
+import { NextRequest, NextResponse } from 'next/server';
+import { getSessionCookie } from 'better-auth/cookies';
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+
+  // This is the recommended approach to optimistically redirect users
+  // but is not a replacement for auth checks in each page/route
+  // (it only checks for the presence of a cookie, not its validity)
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: [
-    "/api/(.*)"
-  ]
-}
+  matcher: ['/api/(.*)'], // Specify the routes the middleware applies to
+};
