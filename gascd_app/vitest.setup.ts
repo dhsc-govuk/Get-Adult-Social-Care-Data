47a3a75a589165a33fb77f26eaa58194f0cc9568
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-10-22 16:07:41 +0100

    Remove unused react vite config

--- gascd_app/vitest.setup.ts
+++ gascd_app/vitest.setup.ts
@@ -1,10 +1,5 @@
 import '@testing-library/jest-dom/vitest';
-import { configure } from '@testing-library/react';
 
 process.env.NEXT_PUBLIC_FEEDBACK_FORM_LINK = 'http://feedback.form.local/';
 process.env.NEXT_PUBLIC_GASCD_GIT_HASH = 'testab1234';
 process.env.NEXT_PUBLIC_GASCD_GIT_TAG = '0.1.0';
-
-configure({
-  asyncUtilTimeout: 5000,
-});
