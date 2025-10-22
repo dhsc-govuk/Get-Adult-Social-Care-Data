15c60e941272c14c5193905bafe1c261bb37aea8
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-10-22 14:54:48 +0100

    Add vitest dependencies and config

---
+++ gascd_app/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    include: ['**/?(*.)+(test).[jt]s?(x)'],
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'vitest.setup.ts',
+    css: true,
+    snapshotSerializers: [],
+    testTimeout: 10000,
+    coverage: {
+      enabled: true,
+      reporter: ['lcov', 'text', 'text-summary'],
+      //thresholds: {
+      //  statements: 90,
+      //  branches: 90,
+      //  functions: 90,
+      //  lines: 90,
+      //},
+      include: ['**/*.ts', '**/*.tsx'],
+      exclude: [
+        '*.d.ts',
+        '.next/**/*',
+        'mock/**/*',
+        'instrumentation.*',
+        '*.config.ts',
+        '**/*.types.ts',
+      ],
+    },
+  },
+});
