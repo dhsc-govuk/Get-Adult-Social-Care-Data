fbbc8df44fe870f241899e969e6beb185581c7cf
Rachel <rachel.dodson@madetech.com>
2025-10-23 11:02:25 +0100

    [GASCD-446] Replace cookie banner with homepage render

--- gascd_app/__tests__/components/CookieBanner/CookieBanner.test.tsx
+++ gascd_app/__tests__/components/CookieBanner/CookieBanner.test.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import CookieBanner from '@/components/common/cookie-banner/CookieBanner';
-import * as mockData from '../../../TestData/ConditionalTextMockData';
-import { mock } from 'node:test';
+import HomePage from '../../../app/(protected)/home/page';
 
 describe('Tests for the cookie banner component', () => {
   it('Renders the cookie banner when no consent cookie is set', () => {
-    // Ensure the consent cookie is not set
-    document.cookie =
-      'GASCDConsentGDPR=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+    document.cookie = 'GASCDConsentGDPR=; expires=; path=/';
 
-    render(<CookieBanner />);
+    render(<HomePage />);
 
     const bannerElement = screen.getByRole('region', {
       name: /Cookies on Get adult social care data/i,
@@ -19,10 +15,9 @@ describe('Tests for the cookie banner component', () => {
   });
 
   it('Does not render the cookie banner when consent cookie is set', () => {
-    // Set the consent cookie
     document.cookie = 'GASCDConsentGDPR=true; path=/';
 
-    render(<CookieBanner />);
+    render(<HomePage />);
 
     const bannerElement = screen.queryByRole('region', {
       name: /Cookies on Get adult social care data/i,
