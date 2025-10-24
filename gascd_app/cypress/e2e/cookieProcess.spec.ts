636224cd0154d97ffeffaeaa188cb1f2429ad79e
Rachel <rachel.dodson@madetech.com>
2025-10-24 14:52:12 +0100

    [GASCD-446] Add tests for cookie page

--- gascd_app/cypress/e2e/cookieProcess.spec.ts
+++ gascd_app/cypress/e2e/cookieProcess.spec.ts
@@ -9,6 +9,7 @@ describe('User can set cookies', () => {
     cy.get('input[type="email"]').type('example@example.com');
     cy.get('input[type="password"]').type(Cypress.env('APPLICATION_PASSWORD'));
     cy.get('button').contains('Sign in with dummy-creds').click();
+    cy.wait(1000);
 
     // Load the homepage
     cy.url().should('include', '/home');
@@ -38,6 +39,7 @@ describe('User can set cookies', () => {
     cy.get('input[type="email"]').type('example@example.com');
     cy.get('input[type="password"]').type(Cypress.env('APPLICATION_PASSWORD'));
     cy.get('button').contains('Sign in with dummy-creds').click();
+    cy.wait(1000);
 
     // Load the homepage
     cy.url().should('include', '/home');
@@ -67,6 +69,7 @@ describe('User can set cookies', () => {
     cy.get('input[type="email"]').type('example@example.com');
     cy.get('input[type="password"]').type(Cypress.env('APPLICATION_PASSWORD'));
     cy.get('button').contains('Sign in with dummy-creds').click();
+    cy.wait(1000);
 
     // Load the homepage
     cy.url().should('include', '/home');
@@ -76,5 +79,36 @@ describe('User can set cookies', () => {
     // Click cookies link
     cy.get('a').contains('View cookies').click();
     cy.url().should('include', '/cookies');
+  });
+
+  it('Should set cookies from cookie page', () => {
+    // Currently assumes local dev auth setup
+    cy.visit('');
+    cy.url().should('include', '/login');
+
+    // Login with dummy auth
+    cy.get('.govuk-button').contains('Agree and sign in').click();
+    cy.get('input[type="email"]').type('example@example.com');
+    cy.get('input[type="password"]').type(Cypress.env('APPLICATION_PASSWORD'));
+    cy.get('button').contains('Sign in with dummy-creds').click();
+
+    // Load the homepage
+    cy.url().should('include', '/home');
+    cy.get('h1').should('contains.text', 'Get adult social care data');
+    cy.get('h2').should('contains.text', 'Cookies');
+
+    // Click cookies link
+    cy.get('a').contains('View cookies').click();
+    cy.url().should('include', '/cookies');
+
+    // Set cookies to accept
+    cy.get('input#radio-yes').check();
+    cy.get('button').contains('Save cookie settings').click();
+    cy.getCookie('GASCDConsentGDPR').should('have.property', 'value', 'true');
+
+    // Set cookies to reject
+    cy.get('input#radio-no').check();
+    cy.get('button').contains('Save cookie settings').click();
+    cy.getCookie('GASCDConsentGDPR').should('have.property', 'value', 'false');
   });
 });
