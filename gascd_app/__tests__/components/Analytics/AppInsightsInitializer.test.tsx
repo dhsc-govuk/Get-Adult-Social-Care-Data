6591280e1a6b5b88e24c4819557438b524dad405
Rachel <rachel.dodson@madetech.com>
2025-10-24 11:14:56 +0100

    [GASCD-446] Split AI test up and add cookie tests

--- gascd_app/__tests__/components/Analytics/AppInsightsInitializer.test.tsx
+++ gascd_app/__tests__/components/Analytics/AppInsightsInitializer.test.tsx
@@ -1,40 +1,21 @@
 import { render } from '@testing-library/react';
 import { AppInsightsInitializer } from '@/components/analytics/AppInsightsInitializer';
-import { getAppInsights } from '@/components/analytics/appInsights';
+import {
+  getAppInsights,
+  resetAppInsights,
+} from '@/components/analytics/appInsights';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import LogService from '@/services/logger/logService';
 
 const TEST_CONNECTION_STRING = 'InstrumentationKey=fake-connection-string';
 
 describe('AppInsightsInitializer', () => {
+  beforeEach(() => {
+    resetAppInsights();
+  });
+
   test('It initialises app insights when called with a valid connection string', () => {
-    document.cookie =
-      'GASCDConsentGDPR=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-
-    render(
-      <AppInsightsInitializer connectionString={TEST_CONNECTION_STRING} />
-    );
-
-    const insights_no_cookies = getAppInsights();
-    expect(insights_no_cookies).toBe(null);
-
     document.cookie = 'GASCDConsentGDPR=true; path=/';
-
-    const insights_initial = getAppInsights();
-    expect(insights_initial).toBe(null);
-
-    render(<AppInsightsInitializer connectionString="" />);
-
-    const insights_blank = getAppInsights();
-    expect(insights_blank).toBe(null);
-
-    const logspy = jest.spyOn(LogService, 'logEvent').mockResolvedValue();
-    render(<AppInsightsInitializer connectionString="blah" />);
-    expect(logspy).toHaveBeenCalledWith(
-      'Error loading browser app insightsError: Please provide instrumentation key'
-    );
-    const insights_invalid = getAppInsights();
-    expect(insights_invalid).toBe(null);
 
     render(
       <AppInsightsInitializer connectionString={TEST_CONNECTION_STRING} />
@@ -44,5 +25,52 @@ describe('AppInsightsInitializer', () => {
     expect(insights).toBeDefined();
     expect(insights).toBeInstanceOf(ApplicationInsights);
     expect(insights?.config.connectionString).toBe(TEST_CONNECTION_STRING);
+  });
+
+  test('It does not initialises app insights when called with an invalid connection string', () => {
+    document.cookie = 'GASCDConsentGDPR=true; path=/';
+
+    const logspy = jest.spyOn(LogService, 'logEvent').mockResolvedValue();
+
+    render(<AppInsightsInitializer connectionString="blah" />);
+
+    expect(logspy).toHaveBeenCalledWith(
+      'Error loading browser app insightsError: Please provide instrumentation key'
+    );
+
+    const insights_invalid = getAppInsights();
+    expect(insights_invalid).toBe(null);
+  });
+
+  test('It does not initialises app insights when called with no connection string', () => {
+    document.cookie = 'GASCDConsentGDPR=true; path=/';
+
+    render(<AppInsightsInitializer connectionString="" />);
+
+    const insights_blank = getAppInsights();
+    expect(insights_blank).toBe(null);
+  });
+
+  test('It does not initialises app insights when called with no cookie consent', () => {
+    document.cookie =
+      'GASCDConsentGDPR=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+
+    render(
+      <AppInsightsInitializer connectionString={TEST_CONNECTION_STRING} />
+    );
+
+    const insights = getAppInsights();
+    expect(insights).toBe(null);
+  });
+
+  test('It does not initialises app insights when called with cookie consent set to false', () => {
+    document.cookie = 'GASCDConsentGDPR=false;';
+
+    render(
+      <AppInsightsInitializer connectionString={TEST_CONNECTION_STRING} />
+    );
+
+    const insights = getAppInsights();
+    expect(insights).toBe(null);
   });
 });
