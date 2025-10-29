fac2bf3f2688116d56be49ce9ecbb4fcfe8e2d24
Rachel <rachel.dodson@madetech.com>
2025-10-29 14:10:49 +0000

    [GASCD-446] Add false test

--- gascd_app/__tests__/services/cookies/CookiesService.test.tsx
+++ gascd_app/__tests__/services/cookies/CookiesService.test.tsx
@@ -6,13 +6,18 @@ describe('CookiesService', () => {
     expect(document.cookie).toBe('GASCDConsentGDPR=true');
   });
 
+  it('Sets the consent cookie to be false', () => {
+    CookiesService.setConsentCookie('false');
+    expect(document.cookie).toBe('GASCDConsentGDPR=false');
+  });
+
   it('Clears analytics cookies', () => {
     document.cookie = 'ai_session=some_value';
     expect(document.cookie).toBe(
-      'GASCDConsentGDPR=true; ai_session=some_value'
+      'GASCDConsentGDPR=false; ai_session=some_value'
     );
 
     CookiesService.removeAnalyticsCookies();
-    expect(document.cookie).toBe('GASCDConsentGDPR=true');
+    expect(document.cookie).toBe('GASCDConsentGDPR=false');
   });
 });
