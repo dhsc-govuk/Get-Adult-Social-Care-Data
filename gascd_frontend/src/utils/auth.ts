c6949ee47bc614eca3bafca63842628dc921bb9f
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-11-05 12:52:38 +0000

    Restore custom user signin logging event

--- gascd_frontend/src/utils/auth.ts
+++ gascd_frontend/src/utils/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth, boolean } from 'better-auth';
+import { createAuthMiddleware } from "better-auth/api";
 import Database from 'better-sqlite3';
 import { B2CPlugin } from './authPlugins';
 import { nextCookies } from 'better-auth/next-js';
+import logger from '@/utils/logger';
 
 export const auth = betterAuth({
   database: new Database('./gascd_users_sqlite.db'),
@@ -32,4 +34,13 @@ export const auth = betterAuth({
     B2CPlugin(),
     nextCookies(), // make sure this is the last plugin in the array
   ],
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if(ctx.context.newSession) {
+        logger.info('User logged in success', {
+          userid: ctx.context.newSession.user.id
+        });
+      }
+    }),
+  },
 });
