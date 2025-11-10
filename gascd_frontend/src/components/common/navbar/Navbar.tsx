58b41074894ae1fbdce8dfc176c3a9d7d3f7d7ca
Rachel Dodson <rachel.dodson@madetech.com>
2025-11-10 17:12:05 +0000

    [501] Add hard coded nav bar component

--- gascd_frontend/src/components/common/navbar/Navbar.tsx
+++ gascd_frontend/src/components/common/navbar/Navbar.tsx
@@ -1,48 +1,125 @@
+import { Session } from 'next-auth';
 import React from 'react';
 
-interface NavbarProps {
-  currentPage: string;
-}
+type Props = {
+  session?: Session | null;
+};
 
-const Navbar: React.FC<NavbarProps> = ({ currentPage }) => {
-  const ActivePage = (page: string) => currentPage === page;
+const Navbar: React.FC<Props> = ({ session }) => {
+
+  const toggleNav = () => {
+    const nav = document.getElementById('navigation');
+    if (nav) {
+      if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+      } else {
+        nav.style.display = 'block';
+      }
+    }
+  };
+
+  const topicLinks = [
+    {
+      name: "Care Homes",
+      description: "Data on residential care homes and nursing homes, including capacity data.",
+      url: "/"
+    },
+    {
+      name: "Community social care",
+      description: "Data on home care, supported living and supported housing providers.",
+      url: "/"
+    },
+    {
+      name: "Funding and unpaid care",
+      description: "Data on local authority funding and support provided by unpaid carers.",
+      url: "/"
+    },
+    {
+      name: "Population needs",
+      description: "Data on a range of care need indicators, including disability prevalence.",
+      url: "/"
+    }
+  ]
+
+  const serviceInformationLinks = [
+    {
+      name: "Homepage",
+      url: "/"
+    },
+    {
+      name: "About this service",
+      url: "/"
+    },
+    {
+      name: "Data indicator details",
+      url: "/"
+    },
+    {
+      name: "User guide",
+      url: "/"
+    },
+  ]
+
+
 
   return (
-    <div
-      className="govuk-service-navigation"
-      data-module="govuk-service-navigation"
-    >
-      <div className="govuk-width-container">
-        <div className="govuk-service-navigation__container">
-          <nav aria-label="Menu" className="govuk-service-navigation__wrapper">
-            <button
-              type="button"
-              className="govuk-service-navigation__toggle govuk-js-service-navigation-toggle"
-              aria-controls="navigation"
-              hidden
-            >
-              Navigation
-            </button>
-            <ul
-              className="govuk-service-navigation__list govuk-service-navigation__item--active"
-              id="navigation"
-            >
-              <li
-                className={`govuk-service-navigation__item ${ActivePage('home') ? 'govuk-service-navigation__item--active' : ''}`}
-              >
-                <a
-                  className="govuk-service-navigation__link"
-                  href="/home"
-                  aria-current="true"
-                >
-                  Home
-                </a>
-              </li>
-            </ul>
-          </nav>
+    <>
+      <section aria-label="Service information" className="govuk-service-navigation" data-module="govuk-service-navigation" data-govuk-service-navigation-init="">
+        <div className="govuk-width-container">
+          <div className="govuk-service-navigation__container">      
+            <span className="govuk-service-navigation__service-name">
+              <a href="/v7/signed-in/home" className="govuk-service-navigation__link">Get adult social care data</a>
+            </span>
+              <ul className="govuk-service-navigation__list">
+                <li className="govuk-service-navigation__item govuk-service-navigation__item--active govuk-service-navigation__item-start">
+                  {localStorage.selectedValue} <a className="govuk-service-navigation__link govuk-service-navigation__link-change" href="/select-location">Change</a>
+                </li>
+                <li className="govuk-service-navigation__item govuk-service-navigation__item-start">
+                  <button aria-controls="super-navigation-menu" aria-label="Show navigation menu" className="govuk-service-navigation__toggle govuk-js-service-navigation-toggle" type="button" onClick={() => toggleNav()}>
+                    Menu
+                  </button>
+                </li>
+              </ul>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+      <nav id="navigation"  className="govuk-service-navigation govuk-!-padding-top-8 govuk-!-padding-bottom-4">
+        <div className="govuk-width-container">
+          <div className="govuk-service-navigation__container">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-two-thirds">
+                <h3 className="govuk-heading-m">Topics</h3>
+                <ul className="govuk-list">
+                  {topicLinks.map((link) => (
+                    <li className="govuk-grid-column-one-half govuk-!-padding-left-0">
+                      <a className="govuk-link govuk-body-s" href={link.url}>
+                        <strong>{link.name}</strong>
+                      </a>
+                      <p className="govuk-body-s">
+                        {link.description}
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="govuk-grid-column-one-third">
+                <h3 className="govuk-heading-m">Service Information</h3>
+                <ul className="govuk-list">
+                  {serviceInformationLinks.map((link) => (
+                    <li className="govuk-!-padding-bottom-3">
+                      <a className="govuk-link govuk-body-s" href={link.url}>
+                        <strong>{link.name}</strong>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
