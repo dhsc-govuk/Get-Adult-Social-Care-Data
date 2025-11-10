9e836d77ce9a5ac95e5abf308485b45b74f1366f
Robert Dunbar <robert.dunbar@madetech.com>
2025-11-10 14:00:55 +0000

    GASCDS-520: fixed scope dictionary and add warning log

--- gascd_api/api/Logging/ApiLogger.cs
+++ gascd_api/api/Logging/ApiLogger.cs
@@ -11,12 +11,18 @@ public class ApiLogger<T>(ILogger<T> logger, IHttpContextAccessor httpContextAcc
         }
     }
 
-    private Dictionary<string, string?> GetScopeDictionary()
+    private Dictionary<string, object> GetScopeDictionary()
     {
         string? traceId = httpContextAccessor.HttpContext?.Items["trace-id"] as string;
         string? requestId = httpContextAccessor.HttpContext?.Items["request-id"] as string;
 
-        return new Dictionary<string, string?> { ["trace-id"] = traceId, ["request-id"] = requestId };
+        if (traceId == null || requestId == null)
+        {
+            Warning("trace-id or request-id is null.");
+            return new Dictionary<string, object>();
+        }
+
+        return new Dictionary<string, object> { ["trace-id"] = traceId, ["request-id"] = requestId };
     }
 
     public void Debug(string msg, params object[] args)
@@ -27,5 +33,10 @@ public class ApiLogger<T>(ILogger<T> logger, IHttpContextAccessor httpContextAcc
     public void Info(string msg, params object[] args)
     {
         Log(LogLevel.Information, msg, args);
+    }
+
+    public void Warning(string msg, params object[] args)
+    {
+        Log(LogLevel.Warning, msg, args);
     }
 }
