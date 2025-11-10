f3908b81dc20c9ba17873eaeaa90e0195322b85a
Robert Dunbar <robert.dunbar@madetech.com>
2025-11-10 11:52:00 +0000

    GASCDS-520: add configuration files

--- gascd_api/api/Program.cs
+++ gascd_api/api/Program.cs
@@ -1,58 +1,23 @@
-using api.Auth;
-using api.Data;
+using api.Configuration;
 using api.Data.Mappers;
-using api.Logging;
-using FastEndpoints;
 using FastEndpoints.Swagger;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.EntityFrameworkCore;
-using NSwag;
 
 var bld = WebApplication.CreateBuilder();
 
-bld.Logging
-    .ClearProviders()
-    .AddJsonConsole(o =>
-        {
-            o.TimestampFormat = "[HH:mm:ss] ";
-            o.IncludeScopes = true;
-        }
-    )
-    .AddDebug()
-    .SetMinimumLevel(LogLevel.Information);
+bld.Logging.RegisterLoggingConfiguration();
 
 bld.Services
-    .AddDbContext<GascdDataContext>(o => o.UseNpgsql(bld.Configuration.GetConnectionString("DefaultConnection")))
-    .AddFastEndpoints()
+    .RegisterDatabase(bld.Configuration)
+    .RegisterFastEndpoints()
     .AddSingleton<PostcodeMapper>()
-    .AddHttpContextAccessor()
-    .AddTransient(typeof(ApiLogger<>))
-    .SwaggerDocument(o =>
-    {
-        o.EnableJWTBearerAuth = false;
-        o.DocumentSettings = s =>
-        {
-            s.AddAuth(ApiKeyAuth.SchemeName, new()
-            {
-                Name = ApiKeyAuth.HeaderName,
-                In = OpenApiSecurityApiKeyLocation.Header,
-                Type = OpenApiSecuritySchemeType.ApiKey
-            });
-        };
-    })
-    .AddAuthorization()
-    .AddAuthentication(ApiKeyAuth.SchemeName)
-    .AddScheme<AuthenticationSchemeOptions, ApiKeyAuth>(ApiKeyAuth.SchemeName, null);
+    .RegisterLoggingServices()
+    .RegisterSwaggerConfiguration()
+    .RegisterAuth();
 
 var app = bld.Build();
 app
-    .UseFastEndpoints(c =>
-    {
-        c.Errors.UseProblemDetails();
-        c.Endpoints.Configurator = ep => ep.PreProcessor<LogPreProcessor>(Order.Before);
-    })
-    .UseAuthentication()
-    .UseAuthorization()
+    .RegisterFastEndpoints()
+    .RegisterAuth()
     .UseSwaggerGen();
 
 app.Run();
