c626745b008fa7ff78781499ae6690730449edc2
Rachel Dodson <rachel.dodson@madetech.com>
2025-11-14 16:42:32 +0000

    [543] Fix test

--- gascd_frontend/cypress/e2e/robots.spec.ts
+++ gascd_frontend/cypress/e2e/robots.spec.ts
@@ -1,6 +1,6 @@
 describe('Site should have a robots.txt file in the root', () => {
   it('Should find the robots.txt file', () => {
 
-    cy.readFile('app/robots.txt').should('eq', 'User-Agent: *\nUser-agent: AdsBot-Google\nDisallow: /');
+    cy.request('/robots.txt').its('body').should('include', 'User-Agent: *\nUser-agent: AdsBot-Google\nDisallow: /')
   });
 });
