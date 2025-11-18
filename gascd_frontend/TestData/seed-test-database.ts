449ebb104bbf41aedb7737e9ed9bfd76b4b947bf
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-11-18 17:29:34 +0000

    Fix DB seed script

--- gascd_frontend/TestData/seed-test-database.ts
+++ gascd_frontend/TestData/seed-test-database.ts
@@ -1,14 +1,14 @@
-import 'dotenv/config'; 
-import { auth } from '../src/utils/auth'; 
+import 'dotenv/config';
+import { auth } from '@/lib/auth';
 
 const seedDevelopmentUser = async () => {
   const email = process.env.LOCAL_AUTH_EMAIL;
   const password = process.env.LOCAL_AUTH_PASSWORD;
-  const name = "Testing User";
+  const name = 'Testing User';
 
   if (!password || !email) {
     console.error(
-      "❌ Error: LOCAL_AUTH_EMAIL and/or LOCAL_AUTH_PASSWORD not set in environment."
+      '❌ Error: LOCAL_AUTH_EMAIL and/or LOCAL_AUTH_PASSWORD not set in environment.'
     );
     process.exit(1);
   }
@@ -24,12 +24,12 @@ const seedDevelopmentUser = async () => {
       },
     });
 
-    console.log(result); 
-    console.log("✅ Development user created successfully!");
+    console.log(result);
+    console.log('✅ Development user created successfully!');
   } catch (error: any) {
-    console.error("❌ Error during seeding:", error.message);
-    if (error.message == "Email and password sign up is not enabled") {
-        console.log("-- Have you set LOCAL_AUTH=true in your environment?")
+    console.error('❌ Error during seeding:', error.message);
+    if (error.message == 'Email and password sign up is not enabled') {
+      console.log('-- Have you set LOCAL_AUTH=true in your environment?');
     }
   }
 };
