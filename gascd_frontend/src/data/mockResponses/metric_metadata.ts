bb427b4f5cd0c889ee68910bbc2e957ae7a80b7f
Rachel Dodson <rachel.dodson@madetech.com>
2025-11-19 09:37:43 +0000

    [394] cut down metric metadata object

--- gascd_frontend/src/data/mockResponses/metric_metadata.ts
+++ gascd_frontend/src/data/mockResponses/metric_metadata.ts
@@ -1,20 +1,8 @@
 export const metric_metadata = {
-  group_id: 'occupancy_rate',
   metric_id: 'occupancy_rate_total',
-  group_name: 'Percentage of adult social care beds occupied',
-  description:
-    'The percentage of occupied beds recorded by care providers across social care compared to the number of beds available.',
-  numerator_description: 'Number of occupied beds',
-  denominator_description: 'Number of beds recorded by care providers',
+  metric_name: 'Total Occupancy Rate',
+  numerator: 'Number of occupied beds',
+  denominator: 'Number of beds recorded by care providers',
   data_source: 'Capacity Tracker',
-  filter_bedtype: 'All bed types',
-  metric_data_type: 'Percentage',
-  methodology: null,
-  limitations:
-    'Capacity Tracker data is self-reported by care providers, who update it at different times throughout the month. This means the data does not provide a snapshot of all providers at the same point in time. Instead, it represents the most recent information available from each provider when the data was retrieved.',
-  access_category: null,
-  metric_type: 'Metric',
-  is_live: '1',
-  deep_dive: '0',
-  load_date_time: '2025-08-29T09:20:27.207Z',
+  data_type: 'Percentage',
 };
