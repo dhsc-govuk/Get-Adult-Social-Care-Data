e795d45b171d87ea397290d61a473e953d5f53a5
Rachel Dodson <rachel.dodson@madetech.com>
2025-11-21 17:08:43 +0000

    [561] Split out DataBox component

--- gascd_frontend/app/(protected)/topics/residential-care/provision-and-occupancy/page.tsx
+++ gascd_frontend/app/(protected)/topics/residential-care/provision-and-occupancy/page.tsx
@@ -7,6 +7,7 @@ import {
   Tabs,
   createAll,
 } from '../../../../../../gascd_frontend/public/govuk-frontend/js/govuk-frontend.min.js';
+import DataBox from '@/components/common/data-box/DataBox';
 
 export default function ProvisionAndOccupancyPage() {
   const breadcrumbs = [
@@ -16,7 +17,7 @@ export default function ProvisionAndOccupancyPage() {
     },
     {
       text: 'Care homes',
-      url: '', //to be added
+      url: '', //todo: update when care homes landing page is created
     },
   ];
 
@@ -47,41 +48,134 @@ export default function ProvisionAndOccupancyPage() {
             </h2>
           </div>
         </div>
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-full">
-            <div className="data-box govuk-form-group">
-              <div className="govuk-tabs" data-module="govuk-tabs">
-                <h2 className="govuk-tabs__title">Contents</h2>
-                <ul className="govuk-tabs__list">
-                  <li className="govuk-tabs__list-item govuk-tabs__list-item--selected">
-                    <a className="govuk-tabs__tab" href="#chart">
-                      Chart
-                    </a>
-                  </li>
-                  <li className="govuk-tabs__list-item">
-                    <a className="govuk-tabs__tab" href="#table">
-                      Table
-                    </a>
-                  </li>
-                </ul>
-                <div className="govuk-tabs__panel" id="chart">
-                  <h2 className="govuk-heading-m">Bar chart</h2>
-                </div>
-                <div
-                  className="govuk-tabs__panel govuk-tabs__panel--hidden"
-                  id="table"
-                >
-                  <h2 id="total" className="govuk-heading-m">
-                    Table
-                  </h2>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
         {/* carehome bed numbers data block */}
+        <DataBox
+          dataTitle="Care home bed numbers"
+          dataInfo={
+            <>
+              Find out{' '}
+              <a
+                href="/help/beds-per-100000-adult-population"
+                className="govuk-link"
+              >
+                how the number of adult social care beds per 100,000 adult
+                population is calculated
+              </a>
+            </>
+          }
+        >
+          <ul className="govuk-tabs__list">
+            <li className="govuk-tabs__list-item govuk-tabs__list-item--selected">
+              <a className="govuk-tabs__tab" href="#chart1">
+                Chart
+              </a>
+            </li>
+            <li className="govuk-tabs__list-item">
+              <a className="govuk-tabs__tab" href="#table1">
+                Table
+              </a>
+            </li>
+            <li className="govuk-tabs__list-item">
+              <a className="govuk-tabs__tab" href="#download1">
+                Download
+              </a>
+            </li>
+          </ul>
+          <div
+            className="govuk-tabs__panel"
+            id="chart1"
+            style={{ backgroundColor: 'white' }}
+          >
+            <h4 className="govuk-heading-s">
+              Figure 1: chart of care home beds per 100,000 adult population –
+              local authorities in the East of England, October 2025
+            </h4>
+            <p className="govuk-body-m">
+              Sources: Capacity Tracker from the Department of Health and Social
+              Care (DHSC), population estimates from the Office for National
+              Statistics (ONS)
+            </p>
+          </div>
+          <div
+            className="govuk-tabs__panel govuk-tabs__panel--hidden"
+            id="table1"
+            style={{ backgroundColor: 'white' }}
+          >
+            <h4 className="govuk-heading-s">
+              Table 1: care home beds per 100,000 adult population for regional
+              local authorities – East of England, October 2025
+            </h4>
+            <p className="govuk-body-m">
+              Sources: Capacity Tracker from the Department of Health and Social
+              Care (DHSC), population estimates from the Office for National
+              Statistics (ONS)
+            </p>
+          </div>
+          <div
+            className="govuk-tabs__panel govuk-tabs__panel--hidden"
+            id="download1"
+            style={{ backgroundColor: 'white' }}
+          >
+            <h4 className="govuk-heading-s">Download</h4>
+          </div>
+        </DataBox>
         {/* occupancy level data block */}
+        <DataBox
+          dataTitle="Occupancy levels"
+          dataInfo={
+            <>
+              Find out how{' '}
+              <a href="/help/percentage-beds-occupied" className="govuk-link">
+                occupancy level percentages
+              </a>{' '}
+              and{' '}
+              <a
+                href="/help/beds-care-provider-location"
+                className="govuk-link"
+              >
+                the number of adult social care beds per 100,000 adult
+                population
+              </a>{' '}
+              are calculated.
+            </>
+          }
+        >
+          <ul className="govuk-tabs__list">
+            <li className="govuk-tabs__list-item">
+              <a className="govuk-tabs__tab" href="#table2">
+                Table
+              </a>
+            </li>
+            <li className="govuk-tabs__list-item">
+              <a className="govuk-tabs__tab" href="#download2">
+                Download
+              </a>
+            </li>
+          </ul>
+          <div
+            className="govuk-tabs__panel govuk-tabs__panel--hidden"
+            id="table2"
+            style={{ backgroundColor: 'white' }}
+          >
+            <h4 className="govuk-heading-s">
+              Table 2: care home bed numbers per 100,000 adult population and
+              occupancy levels – Suffolk local authority, East of England region
+              and England, October 2025
+            </h4>
+            <p className="govuk-body-m">
+              Sources: Capacity Tracker from the Department of Health and Social
+              Care (DHSC), population estimates from the Office for National
+              Statistics (ONS)
+            </p>
+          </div>
+          <div
+            className="govuk-tabs__panel govuk-tabs__panel--hidden"
+            id="download2"
+            style={{ backgroundColor: 'white' }}
+          >
+            <h4 className="govuk-heading-s">Download</h4>
+          </div>
+        </DataBox>
         {/* beds per care home and occupancy level data block */}
         {/* data indicator detains component */}
         {/* local market info component */}
---
+++ gascd_frontend/src/components/common/data-box/DataBox.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+type Props = {
+  dataTitle: string;
+  dataInfo: React.ReactNode;
+  children?: React.ReactNode;
+};
+
+const DataBox: React.FC<Props> = ({ dataTitle, dataInfo, children }) => {
+  return (
+    <div
+      className="govuk-grid-row govuk-!-padding-4 govuk-!-margin-bottom-6"
+      style={{ backgroundColor: ' #f3f2f1' }}
+    >
+      <div className="govuk-grid-column-full govuk-!-padding-0">
+        <div className="data-box govuk-form-group govuk-!-margin-0">
+          <div
+            className="govuk-tabs govuk-!-margin-bottom-0"
+            data-module="govuk-tabs"
+          >
+            <h3 className="govuk-heading-m">{dataTitle}</h3>
+            <p className="govuk-body-m">{dataInfo}</p>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataBox;
