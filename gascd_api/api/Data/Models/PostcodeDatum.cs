9e8e80e388830a3b061ce748389e9143c63317e9
Katherine Hurst <katherine.hurst@madetech.com>
2025-11-28 12:01:36 +0000

    GASCDS-609: add initial create and migration

--- gascd_api/api/Data/Models/PostcodeDatum.cs
+++ gascd_api/api/Data/Models/PostcodeDatum.cs
@@ -1,14 +1,25 @@
-﻿namespace api.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace api.Data.Models;
+
+[Table("postcodes")]
 public partial class PostcodeDatum
 {
+    [Key]
+    [Column("sanitised_postcode")]
     public string SanitisedPostcode { get; init; } = null!;
 
+    [Column("display_postcode")]
     public string DisplayPostcode { get; init; } = null!;
 
+    [Column("latitude")]
     public decimal? Latitude { get; init; }
 
+    [Column("longitude")]
     public decimal? Longitude { get; init; }
 
+    [Column("la_code")]
     public string? LaCode { get; init; }
 }
