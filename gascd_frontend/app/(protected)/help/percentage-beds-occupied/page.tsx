a5eb4509fadb19dfdb7922667142ea0c2cdf1c2f
Rachel Dodson <rachel.dodson@madetech.com>
2025-12-02 12:02:52 +0000

    [590] percent occupancy page

--- gascd_frontend/app/(protected)/help/percentage-beds-occupied/page.tsx
+++ gascd_frontend/app/(protected)/help/percentage-beds-occupied/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Layout from '@/components/common/layout/Layout';
+import DataIndicatorDetails from '@/components/data-components/DataIndicatorDetails';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -8,143 +9,103 @@ const PercentageBedsOccupied: React.FC = () => {
   return (
     <>
       <Layout
-        title="Indicator definition: Percentage of adult social care beds occupied"
+        title="Percentage of adult social care beds occupied"
         showLoginInformation={false}
-        backURL="/present-demand"
+        backURL="/topics/residential-care/provision-and-occupancy/data"
         currentPage={'percentage beds occupied'}
       >
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds">
-            <h1 className="govuk-heading-l">
-              Indicator definition and supporting information: percentage of
-              adult social care beds occupied
-            </h1>
-
-            <p className="govuk-body-l">
-              Find detailed information about this indicator.
+        <DataIndicatorDetails
+          title="Percentage of adult social care beds occupied"
+          whatThisMeasures={
+            <p>
+              The percentage of occupied beds, in relevant bed types, in care
+              providers registered with the Care Quality Commission (CQC).
             </p>
-
-            <table className="govuk-table">
-              <thead className="govuk-table__head">
-                <tr className="govuk-table__row">
-                  <th
-                    scope="row"
-                    className="govuk-table__header govuk-!-width-one-third"
-                  >
-                    What this measures
-                  </th>
-                  <td className="govuk-table__cell">
-                    The percentage of occupied beds, in relevant bed types, in
-                    care providers registered with the Care Quality Commission
-                    (CQC).
-                  </td>
-                </tr>
-              </thead>
-              <tbody className="govuk-table__body">
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Source
-                  </th>
-                  <td className="govuk-table__cell">
-                    <Link
-                      href="https://www.necsu.nhs.uk/digital-applications/capacity-tracker/"
-                      className="govuk-link"
-                      target="_blank"
-                    >
-                      Capacity Tracker (opens in new tab)
-                    </Link>
-                  </td>
-                </tr>
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Update frequency
-                  </th>
-                  <td className="govuk-table__cell">Daily</td>
-                </tr>
-
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Methodology
-                  </th>
-                  <td className="govuk-table__cell">
-                    The occupancy rate is calculated by dividing the number of
-                    occupied beds by the total number of beds. &apos;Beds&apos;
-                    refers to adult social care beds in care providers and
-                    includes the following categories:
-                    <br></br>
-                    <br></br>
-                    <ul className="govuk-list govuk-list--bullet">
-                      <li>dementia nursing</li>
-                      <li>general residential</li>
-                      <li>learning disability residential</li>
-                      <li>YPD - young physically disabled</li>
-                      <li>mental health nursing</li>
-                      <li>learning disability nursing</li>
-                      <li>dementia residential</li>
-                      <li>transitional</li>
-                      <li>mental health residential</li>
-                      <li>general nursing</li>
-                    </ul>
-                    &apos;Occupied beds&apos; refers to beds reported as being
-                    in use at the time of data collection.
-                    <br></br>
-                    <br></br>
-                    Care providers registered with the Care Quality Commission
-                    (CQC) must update this information at least monthly using
-                    the Capacity Tracker tool. The mandated reporting period is
-                    between the 8th and 14th every month, or the next working
-                    day if the 14th falls on a weekend or holiday.
-                    <br></br>
-                    <br></br>
-                    Small number suppression is applied. If fewer than 3 care
-                    provider locations offer a given bed type within a local
-                    authority, region, or country, the value for that area will
-                    be shown as 0. Care provider locations in the Isles of
-                    Scilly are excluded from all aggregated values.
-                  </td>
-                </tr>
-
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Limitations
-                  </th>
-                  <td className="govuk-table__cell">
-                    Care providers may update their Capacity Tracker data at
-                    different times outside the reporting period.
-                    <br></br>
-                    <br></br>
-                    As a result, the data does not provide a snapshot of all
-                    providers at the same time. It reflects the most recent
-                    information available when the data was retrieved.The data
-                    is self-reported and not independently verified.
-                    <br></br>
-                    <br></br>
-                    When adult social care beds are vacant, they can be used
-                    flexibly across a range of bed types. We are exploring ways
-                    to reflect this in the data.
-                    <br></br>
-                    <br></br>
-                    The current bed types are not clearly defined and may be
-                    interpreted differently by care providers submitting data.
-                    To improve consistency, we are working with Capacity Tracker
-                    to explore whether more detailed bed descriptions can be
-                    provided.
-                  </td>
-                </tr>
-
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Data definitions
-                  </th>
-                  <td className="govuk-table__cell">
-                    Refer to the Limitations section for details about work
-                    relating to bed type descriptions.
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+          }
+          source={
+            <>
+              <Link
+                href="https://www.necsu.nhs.uk/digital-applications/capacity-tracker/"
+                className="govuk-link"
+                target="_blank"
+              >
+                Capacity Tracker (opens in new tab)
+              </Link>
+            </>
+          }
+          dataCorrectAsOf={'[Add Data]'}
+          updateFrequency="Daily"
+          methodology={
+            <>
+              <p>
+                The occupancy rate is calculated by dividing the number of
+                occupied beds by the total number of beds. &lsquo;Beds&rsquo;
+                refers to adult social care beds in care providers and includes
+                the following categories:
+              </p>
+              <ul className="govuk-list govuk-list--bullet">
+                <li>general residential</li>
+                <li>general nursing</li>
+                <li>dementia residential</li>
+                <li>dementia nursing</li>
+                <li>mental health residential</li>
+                <li>mental health nursing</li>
+                <li>learning disability residential</li>
+                <li>learning disability nursing</li>
+                <li>transitional care</li>
+                <li>young physically disabled</li>
+              </ul>
+              <p>
+                &lsquo;Occupied beds&rsquo; refers to beds reported as being in
+                use at the time of data collection.
+              </p>
+              <p>
+                Care providers registered with the Care Quality Commission (CQC)
+                must update this information at least monthly using the Capacity
+                Tracker tool. The mandated reporting period is between the 8th
+                and 14th every month, or the next working day if the 14th falls
+                on a weekend or holiday.
+              </p>
+              <p>
+                Small number suppression is applied. Occupied bed counts less
+                than 5 and their corresponding percentages have been suppressed.
+              </p>
+            </>
+          }
+          limitations={
+            <>
+              <p>
+                Care providers may update their Capacity Tracker data at
+                different times outside the reporting period.
+              </p>
+              <p>
+                As a result, the data does not provide a snapshot of all
+                providers at the same time. It reflects the most recent
+                information available when the data was retrieved.
+              </p>
+              <p>
+                The data is self&#45;reported and not independently verified.
+              </p>
+              <p>
+                When adult social care beds are vacant, they can be used
+                flexibly across a range of bed types. We are exploring ways to
+                reflect this in the data.
+              </p>
+              <p>
+                The current bed types are not clearly defined and may be
+                interpreted differently by care providers submitting data. To
+                improve consistency, we are working with Capacity Tracker to
+                explore whether more detailed bed descriptions can be provided.
+              </p>
+            </>
+          }
+          dataDefinitions={
+            <p>
+              Refer to the Limitations section for details about work relating
+              to bed type descriptions.
+            </p>
+          }
+        />
       </Layout>
     </>
   );
