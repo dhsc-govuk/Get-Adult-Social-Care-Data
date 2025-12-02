3ce4682d28ee097c6f2c9d03406ae3e7f70c448e
Rachel Dodson <rachel.dodson@madetech.com>
2025-12-02 11:42:10 +0000

    [590] Beds per 100,000 page

--- gascd_frontend/app/(protected)/help/beds-per-100000-adult-population/page.tsx
+++ gascd_frontend/app/(protected)/help/beds-per-100000-adult-population/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Layout from '@/components/common/layout/Layout';
+import DataIndicatorDetails from '@/components/data-components/DataIndicatorDetails';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -8,152 +9,114 @@ const BedsPer100000AdultPopulation: React.FC = () => {
   return (
     <>
       <Layout
-        title="Indicator definition: Adult social care beds per 100,000 adult population"
+        title="Adult social care beds per 100,000 adult population"
         showLoginInformation={false}
-        backURL="/present-demand"
+        backURL="/topics/residential-care/provision-and-occupancy/data"
         currentPage={'beds per 100,000 adult population'}
       >
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds">
-            <h1 className="govuk-heading-l">
-              Indicator definition and supporting information: adult social care
-              beds per 100,000 adult population
-            </h1>
-
-            <p className="govuk-body-l">
-              Find detailed information about this indicator.
+        <DataIndicatorDetails
+          title="Adult social care beds per 100,000 adult population"
+          whatThisMeasures={
+            <p>
+              The total number of adult social care beds recorded by care
+              providers across health and adult social care, adjusted to a rate
+              of 100,000 adults (aged 18 and over) in the local authority,
+              regional or national population, published by Office for National
+              Statistics.
             </p>
-            <table className="govuk-table">
-              <thead className="govuk-table__head">
-                <tr className="govuk-table__row">
-                  <th
-                    scope="row"
-                    className="govuk-table__header govuk-!-width-one-third"
-                  >
-                    What this measures
-                  </th>
-                  <td className="govuk-table__cell">
-                    The total number of adult social care beds recorded by care
-                    providers across health and adult social care, adjusted to a
-                    rate of 100,000 adults (aged 18 and over) in the local
-                    authority, regional or national population, published by
-                    Office for National Statistics.
-                  </td>
-                </tr>
-              </thead>
-              <tbody className="govuk-table__body">
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Sources
-                  </th>
-                  <td className="govuk-table__cell">
-                    <Link
-                      href="https://www.necsu.nhs.uk/digital-applications/capacity-tracker/"
-                      className="govuk-link"
-                      target="_blank"
-                    >
-                      Capacity Tracker (opens in new tab)
-                    </Link>
-                    <span>, </span>
-                    <Link
-                      href="https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/populationestimatesforenglandandwales/mid2023"
-                      className="govuk-link"
-                      target="_blank"
-                    >
-                      Office for National Statistics (opens in new tab)
-                    </Link>
-                  </td>
-                </tr>
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Update frequency
-                  </th>
-                  <td className="govuk-table__cell">Daily</td>
-                </tr>
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Methodology
-                  </th>
-                  <td className="govuk-table__cell">
-                    This indicator counts all beds reported by care providers
-                    that are related to adult social care, including the
-                    following categories:
-                    <br></br>
-                    <br></br>
-                    <ul className="govuk-list govuk-list--bullet">
-                      <li>dementia nursing</li>
-                      <li>general residential</li>
-                      <li>learning disability residential</li>
-                      <li>YPD - young physically disabled</li>
-                      <li>mental health nursing</li>
-                      <li>learning disability nursing</li>
-                      <li>dementia residential</li>
-                      <li>transitional</li>
-                      <li>mental health residential</li>
-                      <li>general nursing</li>
-                    </ul>
-                    The data covers both self-funded and local authority-funded
-                    beds.
-                    <br></br>
-                    <br></br>
-                    Care providers registered with the Care Quality Commission
-                    (CQC) must update this information at least monthly using
-                    the Capacity Tracker tool. The mandated reporting period is
-                    between the 8th and 14th every month, or the next working
-                    day if the 14th falls on a weekend or holiday.
-                    <br></br>
-                    <br></br>
-                    Small number suppression is applied. If fewer than 3 care
-                    provider locations offer a given bed type within a local
-                    authority, region, or country, the value for that area will
-                    be shown as 0. Care provider locations in the Isles of
-                    Scilly are excluded from all aggregated values.
-                  </td>
-                </tr>
-
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Limitations
-                  </th>
-                  <td className="govuk-table__cell">
-                    Care providers may update their Capacity Tracker data at
-                    different times outside the reporting period.
-                    <br></br>
-                    <br></br>
-                    As a result, the data does not provide a snapshot of all
-                    providers at the same time. It reflects the most recent
-                    information available when the data was retrieved.
-                    <br></br>
-                    <br></br>
-                    The data is self-reported and not independently verified.
-                    <br></br>
-                    <br></br>
-                    When adult social care beds are vacant, they can be used
-                    flexibly across a range of bed types. We are exploring ways
-                    to reflect this in the data.
-                    <br></br>
-                    <br></br>
-                    The current bed types are not clearly defined and may be
-                    interpreted differently by care providers submitting data.
-                    To improve consistency, we are working with Capacity Tracker
-                    to explore whether more detailed bed descriptions can be
-                    provided.
-                  </td>
-                </tr>
-
-                <tr className="govuk-table__row">
-                  <th scope="row" className="govuk-table__header">
-                    Data definitions
-                  </th>
-                  <td className="govuk-table__cell">
-                    Refer to the Limitations section for details about work
-                    relating to bed type descriptions.
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+          }
+          source={
+            <>
+              <Link
+                href="https://www.necsu.nhs.uk/digital-applications/capacity-tracker/"
+                className="govuk-link"
+                target="_blank"
+              >
+                Capacity Tracker (opens in new tab)
+              </Link>
+              <span>, </span>
+              <Link
+                href="https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/populationestimatesforenglandandwales/mid2023"
+                className="govuk-link"
+                target="_blank"
+              >
+                Office for National Statistics (opens in new tab)
+              </Link>
+            </>
+          }
+          dataCorrectAsOf={'WHERE DATA?'}
+          updateFrequency="Daily"
+          methodology={
+            <>
+              <p>
+                This indicator counts all beds reported by care providers that
+                are related to adult social care, including the following
+                categories:
+              </p>
+              <ul className="govuk-list govuk-list--bullet">
+                <li>dementia nursing</li>
+                <li>general residential</li>
+                <li>learning disability residential</li>
+                <li>YPD - young physically disabled</li>
+                <li>mental health nursing</li>
+                <li>learning disability nursing</li>
+                <li>dementia residential</li>
+                <li>transitional</li>
+                <li>mental health residential</li>
+                <li>general nursing</li>
+              </ul>
+              <p>
+                The data covers both self-funded and local authority-funded
+                beds.
+              </p>
+              <p>
+                Care providers registered with the Care Quality Commission (CQC)
+                must update this information at least monthly using the Capacity
+                Tracker tool. The mandated reporting period is between the 8th
+                and 14th every month, or the next working day if the 14th falls
+                on a weekend or holiday.
+              </p>
+              <p>
+                Small number suppression is applied. If fewer than 3 care
+                provider locations offer a given bed type within a local
+                authority, region, or country, the value for that area will be
+                shown as 0. Care provider locations in the Isles of Scilly are
+                excluded from all aggregated values.
+              </p>
+            </>
+          }
+          limitations={
+            <>
+              <p>
+                Care providers may update their Capacity Tracker data at
+                different times outside the reporting period.
+              </p>
+              <p>
+                As a result, the data does not provide a snapshot of all
+                providers at the same time. It reflects the most recent
+                information available when the data was retrieved.
+              </p>
+              <p>The data is self-reported and not independently verified.</p>
+              <p>
+                When adult social care beds are vacant, they can be used
+                flexibly across a range of bed types. We are exploring ways to
+                reflect this in the data.
+              </p>
+              <p>
+                The current bed types are not clearly defined and may be
+                interpreted differently by care providers submitting data. To
+                improve consistency, we are working with Capacity Tracker to
+                explore whether more detailed bed descriptions can be provided.
+              </p>
+            </>
+          }
+          dataDefinitions={
+            <p>
+              Refer to the Limitations section for details about work relating
+              to bed type descriptions.
+            </p>
+          }
+        />
       </Layout>
     </>
   );
