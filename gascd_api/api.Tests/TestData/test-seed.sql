e2962cf8391da740cc8846f6531fd23cac5b87c9
Robert Dunbar <robert.dunbar@madetech.com>
2025-12-02 11:18:28 +0000

    GASCDS-609: limit text fields in schema, amended la to local authority

--- gascd_api/api.Tests/TestData/test-seed.sql
+++ gascd_api/api.Tests/TestData/test-seed.sql
@@ -11,18 +11,39 @@ DROP TABLE IF EXISTS countries;
 DROP TABLE IF EXISTS care_providers;
 
 
+-- Start script
+
+
 CREATE TABLE care_providers (
-                                id text NOT NULL,
-                                name text,
+                                id character varying(15) NOT NULL,
+                                name character varying(100),
                                 loaded_datetime timestamp with time zone NOT NULL,
                                 CONSTRAINT "PK_care_providers" PRIMARY KEY (id)
 );
 
 CREATE TABLE countries (
-                           id text NOT NULL,
-                           name text NOT NULL,
+                           id character varying(15) NOT NULL,
+                           name character varying(50),
                            loaded_datetime timestamp with time zone NOT NULL,
                            CONSTRAINT "PK_countries" PRIMARY KEY (id)
+);
+
+CREATE TABLE regions (
+                         id character varying(15) NOT NULL,
+                         name character varying(50),
+                         country_fk character varying(15),
+                         loaded_datetime timestamp with time zone NOT NULL,
+                         CONSTRAINT "PK_regions" PRIMARY KEY (id),
+                         CONSTRAINT "FK_regions_countries_country_fk" FOREIGN KEY (country_fk) REFERENCES countries (id)
+);
+
+CREATE TABLE local_authorities (
+                                   id character varying(15) NOT NULL,
+                                   name character varying(50),
+                                   region_fk character varying(15),
+                                   loaded_datetime timestamp with time zone NOT NULL,
+                                   CONSTRAINT "PK_local_authorities" PRIMARY KEY (id),
+                                   CONSTRAINT "FK_local_authorities_regions_region_fk" FOREIGN KEY (region_fk) REFERENCES regions (id)
 );
 
 CREATE TABLE postcodes (
@@ -30,50 +51,42 @@ CREATE TABLE postcodes (
                            display_postcode character varying(8) NOT NULL,
                            latitude numeric,
                            longitude numeric,
-                           la_code character varying(9),
+                           local_authority_fk character varying(15),
                            loaded_datetime timestamp with time zone NOT NULL,
-                           CONSTRAINT "PK_postcodes" PRIMARY KEY (sanitised_postcode)
-);
-
-CREATE TABLE regions (
-                         id text NOT NULL,
-                         name text NOT NULL,
-                         country_fk text NOT NULL,
-                         loaded_datetime timestamp with time zone NOT NULL,
-                         CONSTRAINT "PK_regions" PRIMARY KEY (id),
-                         CONSTRAINT "FK_regions_countries_country_fk" FOREIGN KEY (country_fk) REFERENCES countries (id) ON DELETE CASCADE
+                           CONSTRAINT "PK_postcodes" PRIMARY KEY (sanitised_postcode),
+                           CONSTRAINT "FK_postcodes_local_authorities_local_authority_fk" FOREIGN KEY (local_authority_fk) REFERENCES local_authorities (id)
 );
 
 CREATE TABLE care_provider_locations (
-                                         id text NOT NULL,
-                                         name text NOT NULL,
-                                         care_provider_fk text NOT NULL,
-                                         sanitised_postcode_fk character varying(7) NOT NULL,
-                                         address text NOT NULL,
-                                         nominated_individual text NOT NULL,
-                                         local_authority_id text NOT NULL,
+                                         id character varying(15) NOT NULL,
+                                         name character varying(100),
+                                         care_provider_fk character varying(15),
+                                         sanitised_postcode_fk character varying(7),
+                                         address character varying(255),
+                                         nominated_individual character varying(255),
+                                         local_authority_fk character varying(15),
                                          loaded_datetime timestamp with time zone NOT NULL,
                                          CONSTRAINT "PK_care_provider_locations" PRIMARY KEY (id),
-                                         CONSTRAINT "FK_care_provider_locations_care_providers_care_provider_fk" FOREIGN KEY (care_provider_fk) REFERENCES care_providers (id) ON DELETE CASCADE,
-                                         CONSTRAINT "FK_care_provider_locations_postcodes_sanitised_postcode_fk" FOREIGN KEY (sanitised_postcode_fk) REFERENCES postcodes (sanitised_postcode) ON DELETE CASCADE
-);
-
-CREATE TABLE local_authorities (
-                                   id text NOT NULL,
-                                   name text NOT NULL,
-                                   region_fk text NOT NULL,
-                                   loaded_datetime timestamp with time zone NOT NULL,
-                                   CONSTRAINT "PK_local_authorities" PRIMARY KEY (id),
-                                   CONSTRAINT "FK_local_authorities_regions_region_fk" FOREIGN KEY (region_fk) REFERENCES regions (id) ON DELETE CASCADE
+                                         CONSTRAINT "FK_care_provider_locations_care_providers_care_provider_fk" FOREIGN KEY (care_provider_fk) REFERENCES care_providers (id),
+                                         CONSTRAINT "FK_care_provider_locations_local_authorities_local_authority_fk" FOREIGN KEY (local_authority_fk) REFERENCES local_authorities (id),
+                                         CONSTRAINT "FK_care_provider_locations_postcodes_sanitised_postcode_fk" FOREIGN KEY (sanitised_postcode_fk) REFERENCES postcodes (sanitised_postcode)
 );
 
 CREATE INDEX "IX_care_provider_locations_care_provider_fk" ON care_provider_locations (care_provider_fk);
+
+CREATE INDEX "IX_care_provider_locations_local_authority_fk" ON care_provider_locations (local_authority_fk);
 
 CREATE INDEX "IX_care_provider_locations_sanitised_postcode_fk" ON care_provider_locations (sanitised_postcode_fk);
 
 CREATE INDEX "IX_local_authorities_region_fk" ON local_authorities (region_fk);
 
+CREATE INDEX "IX_postcodes_local_authority_fk" ON postcodes (local_authority_fk);
+
 CREATE INDEX "IX_regions_country_fk" ON regions (country_fk);
+
+
+
+-- insert test data
 
 
 INSERT INTO countries (id, name, loaded_datetime)
@@ -86,7 +99,7 @@ INSERT INTO local_authorities (id, name, region_fk, loaded_datetime)
 VALUES ('E08000014', 'Liverpool', 'E12000002', CURRENT_TIMESTAMP);
             
 
-INSERT INTO postcodes (sanitised_postcode, display_postcode, latitude, longitude, la_code, loaded_datetime)
+INSERT INTO postcodes (sanitised_postcode, display_postcode, latitude, longitude, local_authority_fk, loaded_datetime)
 VALUES ('KT220UF', 'KT22 0UF', 51.33954856349381, -0.349629386, 'E08000014', CURRENT_TIMESTAMP),
        ('ME101QX', 'ME10 1QX', 51.32988801568501, 0.7260691453143282, 'E08000014', CURRENT_TIMESTAMP),
        ('ME101QY', 'ME10 1QY', 51.32954649874547, 0.7283745919593453, 'E08000014', CURRENT_TIMESTAMP),
--- gascd_api/api/Data/Models/CareProviderLocation.cs
+++ gascd_api/api/Data/Models/CareProviderLocation.cs
@@ -6,11 +6,10 @@ namespace api.Data.Models;
 [Table("care_provider_locations")]
 public class CareProviderLocation
 {
-    [Key]
-    [Column("id")]
+    [Key, Column("id"), StringLength(15)]
     public string? Id { get; init; }
 
-    [Column("name")]
+    [Column("name"), StringLength(100)]
     public string? Name { get; init; }
 
     [Column("care_provider_fk")]
@@ -25,14 +24,17 @@ public class CareProviderLocation
     [ForeignKey("SanitisedPostcodeFk")]
     public virtual Postcode? Postcode { get; init; }
 
-    [Column("address")]
+    [Column("address"), StringLength(255)]
     public string? Address { get; init; }
 
-    [Column("nominated_individual")]
+    [Column("nominated_individual"), StringLength(255)]
     public string? NominatedIndividual { get; init; }
 
-    [Column("local_authority_id")]
-    public string? LocalAuthorityId { get; init; }
+    [Column("local_authority_fk")]
+    public string? LocalAuthorityFk { get; init; }
+
+    [ForeignKey("LocalAuthorityFk")]
+    public virtual LocalAuthority? LocalAuthority { get; init; }
 
     [Column("loaded_datetime"), DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTime LoadedDateTime { get; init; }
