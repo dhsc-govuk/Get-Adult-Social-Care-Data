7182a101ed5978965c85ba74630f55bd9ce35c73
Rachel Dodson <rachel.dodson@madetech.com>
2025-12-03 16:12:37 +0000

    [590] dementia and 4th dimension deprived

--- gascd_frontend/app/(protected)/help/dementia-prevalence/page.tsx
+++ gascd_frontend/app/(protected)/help/dementia-prevalence/page.tsx
@@ -19,41 +19,35 @@ const DementiaPrevalence = () => {
             </p>
           }
           source={
-            <>
-              <Link
-                href="https://fingertips.phe.org.uk/dementia#page/6/gid/1938132811/pat/159/par/K02000001/ati/15/are/E92000001/iid/247/age/1/sex/4/cat/-1/ctp/-1/yrr/1/cid/4/tbm/1/page-options/car-do-0"
-                className="govuk-link"
-                target="_blank"
-              >
-                Fingertips from the Department of Health and Social Care (opens
-                in new tab)
-              </Link>
-            </>
+            <Link
+              href="https://fingertips.phe.org.uk/dementia#page/6/gid/1938132811/pat/159/par/K02000001/ati/15/are/E92000001/iid/247/age/1/sex/4/cat/-1/ctp/-1/yrr/1/cid/4/tbm/1/page-options/car-do-0"
+              className="govuk-link"
+              target="_blank"
+            >
+              Fingertips from the Department of Health and Social Care (opens in
+              new tab)
+            </Link>
           }
           dataCorrectAsOf={'[Add Data]'}
           updateFrequency="Yearly"
           methodology={
-            <>
-              <p>
-                Based on Quality and Outcomes Framework (QOF) data, this measure
-                calculates the proportion of patients on GP practice registers
-                with a recorded diagnosis of dementia. Figures are aggregated to
-                local authority level using the postcode of the GP practice, not
-                the patient&apos;s home address.
-              </p>
-            </>
+            <p>
+              Based on Quality and Outcomes Framework (QOF) data, this measure
+              calculates the proportion of patients on GP practice registers
+              with a recorded diagnosis of dementia. Figures are aggregated to
+              local authority level using the postcode of the GP practice, not
+              the patient&apos;s home address.
+            </p>
           }
           limitations={
-            <>
-              <p>
-                The measure is intended to show the prevalence of dementia among
-                residents of each local authority area. However, data by place
-                of residence is not available. Instead, a proxy is used based on
-                GP registrations, assigning individuals to a local authority
-                according to the postcode of their GP practice rather than their
-                home address.
-              </p>
-            </>
+            <p>
+              The measure is intended to show the prevalence of dementia among
+              residents of each local authority area. However, data by place of
+              residence is not available. Instead, a proxy is used based on GP
+              registrations, assigning individuals to a local authority
+              according to the postcode of their GP practice rather than their
+              home address.
+            </p>
           }
           dataDefinitions={<p>See data source for details.</p>}
         />
--- gascd_frontend/app/(protected)/help/households-deprived-4-dimensions/page.tsx
+++ gascd_frontend/app/(protected)/help/households-deprived-4-dimensions/page.tsx
@@ -1,87 +1,50 @@
 import Layout from '@/components/common/layout/Layout';
+import DataIndicatorDetails from '@/components/data-components/DataIndicatorDetails';
+import Link from 'next/link';
 
 const HouseholdsDeprived = () => {
   return (
     <>
       <Layout
-        title="Indicator definition and supporting information: households 'deprived in 4 dimensions'"
+        title="Households deprived in 4 dimensions"
         backURL="/present-demand"
       >
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds">
-            <h1 className="govuk-heading-l">
-              Indicator definition and supporting information: households
-              &apos;deprived in 4 dimensions&apos;
-            </h1>
-            <p className="govuk-body-l">
-              Find detailed information about this indicator.
+        <DataIndicatorDetails
+          title="Households deprived in 4 dimensions"
+          whatThisMeasures={
+            <p>
+              The percentage of households deprived in all four Census-defined
+              dimensions: employment (no one in the household in work),
+              education (no household member with qualifications and no one aged
+              16 to 18 in full-time education), health and disability (any
+              member reporting bad health or long-term illness), and housing
+              (overcrowding, shared facilities or lack of central heating)
+              within the selected administrative area within England.
             </p>
-            <dl className="govuk-summary-list">
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">What this measures</dt>
-                <dd className="govuk-summary-list__value">
-                  The percentage of households deprived in all four
-                  Census-defined dimensions: employment (no one in the household
-                  in work), education (no household member with qualifications
-                  and no one aged 16 to 18 in full-time education), health and
-                  disability (any member reporting bad health or long-term
-                  illness), and housing (overcrowding, shared facilities or lack
-                  of central heating) within the selected administrative area
-                  within England.
-                </dd>
-              </div>
-
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Source</dt>
-                <dd className="govuk-summary-list__value">
-                  <a
-                    href="https://www.nomisweb.co.uk/datasets/c2021ts011"
-                    className="govuk-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Census 2021 from the Office for National Statistics (opens
-                    in new tab)
-                  </a>
-                </dd>
-              </div>
-
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Data correct as of</dt>
-                <dd className="govuk-summary-list__value">21 March 2021</dd>
-              </div>
-
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Update frequency</dt>
-                <dd className="govuk-summary-list__value">Census cadence</dd>
-              </div>
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Methodology</dt>
-                <dd className="govuk-summary-list__value">
-                  This dataset provides Census 2021 estimates that classify
-                  households in England and Wales by four dimensions of
-                  deprivation: employment, education, health and disability, and
-                  household overcrowding. The estimates are as at Census Day, 21
-                  March 2021.
-                </dd>
-              </div>
-
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Limitations</dt>
-                <dd className="govuk-summary-list__value">
-                  See data source for details.
-                </dd>
-              </div>
-
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Data definitions</dt>
-                <dd className="govuk-summary-list__value">
-                  See data source for details.
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
+          }
+          source={
+            <Link
+              href="https://www.nomisweb.co.uk/datasets/c2021ts011"
+              className="govuk-link"
+              target="_blank"
+            >
+              Census 2021 from the Office for National Statistics (opens in new
+              tab)
+            </Link>
+          }
+          dataCorrectAsOf={'[Add Data]'}
+          updateFrequency="Census cadence"
+          methodology={
+            <p>
+              This dataset provides Census 2021 estimates that classify
+              households in England and Wales by four dimensions of deprivation:
+              employment, education, health and disability, and household
+              overcrowding. The estimates are as at Census Day, 21 March 2021.
+            </p>
+          }
+          limitations={<p>See data source for details.</p>}
+          dataDefinitions={<p>See data source for details.</p>}
+        />
       </Layout>
     </>
   );
