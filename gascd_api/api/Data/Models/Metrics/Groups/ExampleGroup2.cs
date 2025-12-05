2a42c8c62e1c4bd173747f5ce67aab90f61dfc62
Katherine Hurst <katherine.hurst@madetech.com>
2025-12-05 16:01:53 +0000

    GASCDS-609: Add metric schema and update DbContext accordingly

---
+++ gascd_api/api/Data/Models/Metrics/Groups/ExampleGroup2.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Data.Models.Metrics.Groups;
+
+[Table("example_group_2")]
+public class ExampleGroup2 : Metric
+{
+
+}
---
+++ gascd_api/api/Data/Models/Metrics/MetricGroup.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Data.Models.Metrics;
+
+[Table("metric_groups")]
+public class MetricGroup : EntityBase
+{
+    [Column("name"), StringLength(100)]
+    public required string Name { get; init; }
+
+}
