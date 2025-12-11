0d5234e32b27074684db8246140160b08a97eede
Katherine Hurst <katherine.hurst@madetech.com>
2025-12-11 11:40:08 +0000

    GASCDS-638: add endpoint logging

--- gascd_api/api/Endpoints/Organisation/CareProvider/GetCareProviderEndpoint.cs
+++ gascd_api/api/Endpoints/Organisation/CareProvider/GetCareProviderEndpoint.cs
@@ -4,7 +4,7 @@ using FastEndpoints;
 
 namespace api.Endpoints.Organisation.CareProvider;
 
-public class GetCareProviderEndpoint(GascdDataContext context, ReferenceMapper mapper) : Endpoint<GetCareProviderRequest, List<GetCareProviderResponse>>
+public class GetCareProviderEndpoint(GascdDataContext context, ReferenceMapper mapper, ILogger<GetCareProviderEndpoint> logger) : Endpoint<GetCareProviderRequest, List<GetCareProviderResponse>>
 {
     public override void Configure()
     {
@@ -13,18 +13,20 @@ public class GetCareProviderEndpoint(GascdDataContext context, ReferenceMapper m
 
     public override async Task HandleAsync(GetCareProviderRequest req, CancellationToken ct)
     {
+        logger.LogDebug("Received request for care provider code: {code}", req.CareProviderCode);
         var locations = context.CareProviderLocations
             .Where(cpl => cpl.CareProvider.Code == req.CareProviderCode);
 
         if (!locations.Any() && !context.CareProviders.Any(cp => cp.Code == req.CareProviderCode))
         {
+            logger.LogInformation("Care provider code not found: {code}", req.CareProviderCode);
             await Send.NotFoundAsync(ct);
             return;
         }
 
         List<GetCareProviderResponse> response =
             locations.Select(l => mapper.CareProviderLocationToCareProviderLocationResponse(l)).ToList();
-
+        logger.LogInformation("Finished processing care provider code: {code}", req.CareProviderCode);
         await Send.OkAsync(response, ct);
     }
 }
