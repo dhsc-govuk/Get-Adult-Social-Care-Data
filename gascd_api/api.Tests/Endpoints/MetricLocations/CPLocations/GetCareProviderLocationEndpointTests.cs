5cd8569c91a30279438bedabd5ce885d23aa0655
Katherine Hurst <katherine.hurst@madetech.com>
2025-12-12 15:48:41 +0000

    GASCDS-533: add test to check JSON content of endpoint

--- gascd_api/api.Tests/Endpoints/MetricLocations/CPLocations/GetCareProviderLocationEndpointTests.cs
+++ gascd_api/api.Tests/Endpoints/MetricLocations/CPLocations/GetCareProviderLocationEndpointTests.cs
@@ -1,8 +1,10 @@
 using api.Endpoints.MetricLocation.CpLocations;
 using api.Tests.Fixtures;
 using FastEndpoints;
+using Newtonsoft.Json.Linq;
 using Shouldly;
 using System.Net;
+using static api.Tests.Fixtures.TestUtils;
 
 namespace api.Tests.Endpoints.MetricLocations.CPLocations;
 
@@ -42,6 +44,25 @@ public class GetCareProviderLocationEndpointTests : IClassFixture<IntegrationTes
         response.NominatedIndividual.ShouldBe("Mr. Ice Cool");
         response.LocalAuthorityId.ShouldBe("E08000014");
     }
+
+    [Fact]
+    public async Task GetCareProviderLocation_ReturnsExpectedCareProviderJsonObject()
+    {
+        var response = await _client.GetAsync("api/metric_locations/cp_locations/1-222222222", TestContext.Current.CancellationToken);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var jsonArray = await ParseJsonResponse<JObject>(response);
+        GetFromJson(jsonArray, "id").ShouldBe("1-222222222");
+        GetFromJson(jsonArray, "display_name").ShouldBe("Bupa Liverpool");
+        GetFromJson(jsonArray, "address").ShouldBe("Bupa Liverpool, CV2 2TN");
+        GetFromJson(jsonArray, "provider_id").ShouldBe("1-123456789");
+        GetFromJson(jsonArray, "provider_name").ShouldBe("Bupa");
+        GetFromJson(jsonArray, "nominated_individual").ShouldBe("Mr. Ice Cool");
+        GetFromJson(jsonArray, "local_authority_id").ShouldBe("E08000014");
+
+    }
+
+
+
 
 
 
