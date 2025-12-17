37580f3121b578e3706265cd7f86e86062bf14e8
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2025-12-17 12:01:15 +0000

    Fix analytics tests

--- gascd_frontend/__tests__/services/analytics/AnalyticsService.test.tsx
+++ gascd_frontend/__tests__/services/analytics/AnalyticsService.test.tsx
@@ -1,27 +1,29 @@
-import AnalyticsService from "@/services/analytics/analyticsService";
-import { initializeAppInsights, getAppInsights } from "@/components/analytics/appInsights";
+import AnalyticsService from '@/services/analytics/analyticsService';
+import {
+  initializeAppInsights,
+  getAppInsights,
+} from '@/components/analytics/appInsights';
 
 const TEST_CONNECTION_STRING = 'InstrumentationKey=fake-connection-string';
 
 describe('AnalyticsService', () => {
   it('does not fail if no analytics are set up', () => {
-    AnalyticsService.trackMetricLocationView("mylocationid");
+    AnalyticsService.trackMetricView('my-metric');
   });
 
   it('tracks events with the correct location', () => {
     initializeAppInsights(TEST_CONNECTION_STRING);
     const appInsights = getAppInsights() as any;
-    const insightSpy = vi.spyOn(appInsights, 'trackEvent')
+    const insightSpy = vi.spyOn(appInsights, 'trackEvent');
 
-    AnalyticsService.trackMetricLocationView("mylocationid");
+    AnalyticsService.trackMetricView('my-metric');
 
     const expected_event = {
-     "name": "location_metrics_view",
-     "properties": {
-        "activeLocationId": "mylocationid",
-     },
-    }
+      name: 'metric-view',
+      properties: {
+        metric_id: 'my-metric',
+      },
+    };
     expect(insightSpy).toHaveBeenCalledExactlyOnceWith(expected_event);
   });
-
 });
