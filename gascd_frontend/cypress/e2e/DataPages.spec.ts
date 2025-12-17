60586f43e42a89567ab6f58386c487b159941f68
Rachel Dodson <rachel.dodson@madetech.com>
2025-12-17 15:57:34 +0000

    [674] Add rest of pages

--- gascd_frontend/cypress/e2e/DataPages.spec.ts
+++ gascd_frontend/cypress/e2e/DataPages.spec.ts
@@ -80,6 +80,7 @@ describe('Data pages exist and contain data', () => {
     cy.get('a[href*="download-3"]').click();
     cy.url().should('include', '#download-3');
     cy.get('#download-3').should('be.visible');
+    cy.wait(500);
     cy.get('#textSummary-3').should('not.be.visible');
     cy.get('#table-3').should('not.be.visible');
 
@@ -112,15 +113,142 @@ describe('Data pages exist and contain data', () => {
     cy.get('#table-1')
       .should('be.visible')
       .contains('Table 1: population size and age group percentages');
+    cy.get('#textSummary-1').should('not.be.visible');
+    cy.get('#download-1').should('not.be.visible');
 
     cy.get('a[href*="textSummary-1"]').click();
+    cy.wait(500);
     cy.url().should('include', '#textSummary-1');
     cy.get('#textSummary-1')
       .should('be.visible')
       .contains('This suggests the need for care services');
+    cy.get('#table-1').should('not.be.visible');
+    cy.get('#download-1').should('not.be.visible');
 
     cy.get('a[href*="download-1"]').click();
     cy.url().should('include', '#download-1');
     cy.get('#download-1').should('be.visible');
+    cy.get('#textSummary-1').should('not.be.visible');
+    cy.get('#table-1').should('not.be.visible');
+  });
+
+  it('Should navigate to the Economic factors and household composition page and check the contents', () => {
+    cy.login();
+    cy.visit(
+      '/topics/population-needs/household-composition-and-economic-factors/data'
+    );
+
+    cy.get('h1').should(
+      'contains.text',
+      'Economic factors and household composition'
+    );
+
+    cy.contains('Household deprivation');
+
+    cy.get('a[href*="table-1"]').click();
+    cy.url().should('include', '#table-1');
+    cy.get('#table-1')
+      .should('be.visible')
+      .contains('Table 1: percentage of households classified as');
+    cy.get('#download-1').should('not.be.visible');
+
+    cy.get('a[href*="download-1"]').click();
+    cy.url().should('include', '#download-1');
+    cy.get('#download-1').should('be.visible');
+    cy.get('#table-1').should('not.be.visible');
+
+    cy.contains('Households where the property is owned outright');
+
+    cy.get('a[href*="table-2"]').click();
+    cy.url().should('include', '#table-2');
+    cy.get('#table-2')
+      .should('be.visible')
+      .contains(
+        'Table 2: percentage of households where the property is owned outright'
+      );
+    cy.get('#download-2').should('not.be.visible');
+
+    cy.get('a[href*="download-2"]').click();
+    cy.url().should('include', '#download-2');
+    cy.get('#download-2').should('be.visible');
+    cy.get('#table-2').should('not.be.visible');
+
+    cy.contains('One-person households where the person is aged 65 or over');
+
+    cy.get('a[href*="table-3"]').click();
+    cy.url().should('include', '#table-3');
+    cy.get('#table-3')
+      .should('be.visible')
+      .contains(
+        'Table 3: percentage of one-person households where the person is aged 65 or over'
+      );
+    cy.get('#download-3').should('not.be.visible');
+
+    cy.get('a[href*="download-3"]').click();
+    cy.url().should('include', '#download-3');
+    cy.get('#download-3').should('be.visible');
+    cy.get('#table-3').should('not.be.visible');
+  });
+
+  it('Should navigate to the General health, disability and learning disability page and check the contents', () => {
+    cy.login();
+    cy.visit('/topics/population-needs/disability-prevalence/data');
+
+    cy.get('h1').should(
+      'contains.text',
+      'General health, disability and learning disability'
+    );
+
+    cy.contains('Disability prevalence');
+
+    cy.get('a[href*="table-1"]').click();
+    cy.url().should('include', '#table-1');
+    cy.get('#table-1')
+      .should('be.visible')
+      .contains('Table 1: self-reporting on general health and disability');
+    cy.get('#download-1').should('not.be.visible');
+
+    cy.get('a[href*="download-1"]').click();
+    cy.url().should('include', '#download-1');
+    cy.get('#download-1').should('be.visible');
+    cy.get('#table-1').should('not.be.visible');
+
+    cy.contains('Learning disability prevalence');
+
+    cy.get('a[href*="table-2"]').click();
+    cy.url().should('include', '#table-2');
+    cy.get('#table-2')
+      .should('be.visible')
+      .contains('Table 2: learning disability prevalence');
+    cy.get('#download-2').should('not.be.visible');
+
+    cy.get('a[href*="download-2"]').click();
+    cy.url().should('include', '#download-2');
+    cy.get('#download-2').should('be.visible');
+    cy.get('#table-2').should('not.be.visible');
+  });
+
+  it('Should navigate to the Dementia prevalence and estimated diagnosis rate page and check the contents', () => {
+    cy.login();
+    cy.visit('/topics/population-needs/dementia-prevalence/data');
+
+    cy.get('h1').should(
+      'contains.text',
+      'Dementia prevalence and estimated diagnosis rate'
+    );
+
+    cy.contains('Dementia prevalence and estimated diagnosis rate');
+
+    cy.get('a[href*="table-1"]').click();
+    cy.url().should('include', '#table-1');
+    cy.get('#table-1')
+      .should('be.visible')
+      .contains('Table 1: dementia prevalence and the dementia diagnosis rate');
+    cy.get('#download-1').should('not.be.visible');
+
+    cy.get('a[href*="download-1"]').click();
+    cy.url().should('include', '#download-1');
+    cy.get('#download-1').should('be.visible');
+    cy.get('#table-1').should('not.be.visible');
   });
 });
