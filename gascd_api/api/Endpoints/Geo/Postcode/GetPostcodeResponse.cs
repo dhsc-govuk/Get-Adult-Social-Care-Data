31d7a5dbc5ffdb746ca5c8c5c6dda40783c69323
Katherine Hurst <katherine.hurst@madetech.com>
2025-12-22 14:53:11 +0000

    GASCDS-679: removed latitude and longitude from response

--- gascd_api/api/Endpoints/Geo/Postcode/GetPostcodeResponse.cs
+++ gascd_api/api/Endpoints/Geo/Postcode/GetPostcodeResponse.cs
@@ -7,8 +7,6 @@ public class GetPostcodeResponse
     public required string SanitisedPostcode { get; init; }
     public required string DisplayPostcode { get; init; }
     public required GeoDataDto GeoData { get; init; }
-    public double? Latitude { get; init; }
-    public double? Longitude { get; init; }
     public string? LaCode { get; init; }
     public string? LaName { get; init; }
 }
