44c613a623376248371cd9084c5defe944019841
Katherine Hurst <katherine.hurst@madetech.com>
2026-01-05 16:19:50 +0000

    GASCDS-705: add validator unit tests

---
+++ gascd_api/api.Tests/Endpoints/MetricLocations/Countries/GetCountryValidatorTests.cs
@@ -0,0 +1,42 @@
+using api.Endpoints.MetricLocation.Countries;
+using FluentValidation.TestHelper;
+
+namespace api.Tests.Endpoints.MetricLocations.Countries;
+
+public class GetCountryValidatorTests : IDisposable
+{
+    private GetCountryValidator _validator;
+
+    public GetCountryValidatorTests()
+    {
+        _validator = new GetCountryValidator();
+    }
+
+    [Theory]
+    [InlineData("E11")]
+    [InlineData("E1123456789")]
+    [InlineData("E41234567891011")]
+    public void ValidCountryCode_ShouldBeValid(string laCode)
+    {
+        var request = new GetCountryRequest { CountryCode = laCode };
+        var result = _validator.TestValidate(request);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [InlineData("E1", "Country code has a minimum length of 3")]
+    [InlineData(" ", "Country code is required")]
+    [InlineData("E1123233223243278910111", "Country code has a maximum length of 15")]
+    public void InvalidCountryCode_ShouldBeInvalid(string regionCode, string expectedErrorMessage)
+    {
+        var request = new GetCountryRequest { CountryCode = regionCode };
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(r => r.CountryCode)
+            .WithErrorMessage(expectedErrorMessage);
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
