59d0989792875da9d7dac642b743fab895686593
Robert Dunbar <robert.dunbar@madetech.com>
2026-01-07 14:52:16 +0000

    GASCDS-707: add logging

--- gascd_api/api/Endpoints/MetricFilters/GetMetricFiltersEndpoint.cs
+++ gascd_api/api/Endpoints/MetricFilters/GetMetricFiltersEndpoint.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.Endpoints.MetricFilters;
 
-public class GetMetricFiltersEndpoint(GascdDataContext context, ReferenceMapper mapper) : Endpoint<GetMetricFiltersRequest, GetMetricFiltersResponse>
+public class GetMetricFiltersEndpoint(GascdDataContext context, ReferenceMapper mapper, ILogger<GetMetricFiltersEndpoint> logger) : Endpoint<GetMetricFiltersRequest, GetMetricFiltersResponse>
 {
     public override void Configure()
     {
@@ -14,19 +14,20 @@ public class GetMetricFiltersEndpoint(GascdDataContext context, ReferenceMapper 
 
     public override async Task HandleAsync(GetMetricFiltersRequest req, CancellationToken ct)
     {
+        logger.LogDebug("Received request for metric group code: {code}", req.MetricGroupCode);
         var metricGroup = context.MetricGroups
             .Include(mg => mg.Metrics)
             .SingleOrDefault(cp => cp.Code == req.MetricGroupCode);
 
         if (metricGroup == null)
         {
+            logger.LogInformation("Metric group code not found: {code}", req.MetricGroupCode);
             await Send.NotFoundAsync(ct);
             return;
         }
 
         var response = mapper.MetricGroupToMetricFiltersResponse(metricGroup);
-
+        logger.LogInformation("Finished processing metric group code: {code}", req.MetricGroupCode);
         await Send.OkAsync(response, ct);
-
     }
 }
