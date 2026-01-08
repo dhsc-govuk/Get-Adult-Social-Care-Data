04d87d342f8c387f940b8ef745d0b66374d43548
Rachel Dodson <rachel.dodson@madetech.com>
2026-01-08 16:43:05 +0000

    [394] Catch up with packages

--- gascd_frontend/__tests__/handlers/location.test.ts
+++ gascd_frontend/__tests__/handlers/location.test.ts
@@ -11,41 +11,43 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('test handlers', () => {
+  it('fetches and returns care provider locations successfully', async () => {
+    const query = 'testcpl1';
+    const mockCPLocation: {} = locations_data.care_provider_location;
 
-    it('fetches and returns care provider locations successfully', async () => {
-        const query = 'testcpl1';
-        const mockCPLocation: {} = locations_data.care_provider_location;
+    const req = {
+      url: `http://localhost/api/get_care_provider?cp_code=${query}`,
+    };
 
-        const req = { url: `http://localhost/api/get_care_provider?cp_code=${query}` } as NextRequest;
+    const result = await GetCareProviderLocation(req);
+    const data = await result.json();
 
-        const result = await GetCareProviderLocation(req);
-        const data = await result.json();
+    expect(data).toEqual(mockCPLocation);
+  });
 
-        expect(data).toEqual(mockCPLocation);
-    });
+  it('fetches and returns local authority successfully', async () => {
+    const query = 'E08000024';
+    const mockLocalAuthority: {} = locations_data.local_authority;
 
-    it('fetches and returns local authority successfully', async () => {
-        const query = 'E08000024';
-        const mockLocalAuthority: {} = locations_data.local_authority;
+    const req = {
+      url: `http://localhost/api/get_local_authority?la_code=${query}`,
+    };
 
-        const req = { url: `http://localhost/api/get_local_authority?la_code=${query}` } as NextRequest;
+    const result = await GetLocalAuthority(req);
+    const data = await result.json();
 
-        const result = await GetLocalAuthority(req);
-        const data = await result.json();
+    expect(data).toEqual(mockLocalAuthority);
+  });
 
-        expect(data).toEqual(mockLocalAuthority);
-    });
+  it('fetches and returns regions successfully', async () => {
+    const query = 'E12000001';
+    const mockRegion: {} = locations_data.region;
 
-    it('fetches and returns regions successfully', async () => {
-        const query = 'E12000001';
-        const mockRegion: {} = locations_data.region;
+    const req = { url: `http://localhost/api/get_region?region_code=${query}` };
 
-        const req = { url: `http://localhost/api/get_region?region_code=${query}` } as NextRequest;
+    const result = await GetRegion(req);
+    const data = await result.json();
 
-        const result = await GetRegion(req);
-        const data = await result.json();
-
-        expect(data).toEqual(mockRegion);
-    });
-
+    expect(data).toEqual(mockRegion);
+  });
 });
