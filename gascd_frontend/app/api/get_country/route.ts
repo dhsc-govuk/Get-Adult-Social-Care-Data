55a2ddf569e6e83dfee2acefe09aa6308dd6cd96
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-15 17:00:01 +0000

    Remove extra error logging

--- gascd_frontend/app/api/get_country/route.ts
+++ gascd_frontend/app/api/get_country/route.ts
@@ -21,7 +21,6 @@ export async function GET(req: NextRequest) {
           },
         },
       });
-      console.error(data);
       if (!data) {
         return NextResponse.json({ error: 'No data found' }, { status: 404 });
       }
