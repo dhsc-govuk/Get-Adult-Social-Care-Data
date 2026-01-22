b96ce6230453d12661042b1d1972a79a23ad84f8
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-22 16:46:09 +0000

    Plug metric filters route into the API

--- gascd_frontend/app/api/get_all_total_beds_filters/route.ts
+++ gascd_frontend/app/api/get_all_total_beds_filters/route.ts
@@ -2,8 +2,38 @@ import { NextRequest, NextResponse } from 'next/server';
 import { dbPool } from '../../../src/data/dbModule';
 import { TotalBedsFilters } from '@/data/interfaces/TotalBedsFilters';
 import logger from '@/utils/logger';
+import { getAPIClient } from '@/data/dataAPI';
 
 export async function GET(req: NextRequest) {
+  const metric_group_code = 'bedcount_per_100000_adults';
+  if (process.env.DATA_API_ROOT) {
+    const client = getAPIClient();
+    const { data, error } = await client.GET(
+      '/metric_filters/{metric_group_code}',
+      {
+        params: {
+          path: {
+            metric_group_code: metric_group_code,
+          },
+        },
+      }
+    );
+    if (data && data.metrics) {
+      const rows = data.metrics.map((row) => ({
+        metric_id: row.metric_code,
+        filter_bedtype: row.display_name,
+        checked: false,
+      }));
+      return NextResponse.json(rows);
+    } else {
+      logger.error(`No filters found for ${metric_group_code}`);
+      return NextResponse.json(
+        { error: `Error fetching filters data` },
+        { status: 500 }
+      );
+    }
+  }
+
   try {
     const pool = await dbPool;
     const resultSet = await pool
