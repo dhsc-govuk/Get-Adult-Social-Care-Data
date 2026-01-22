a7e5ba2914b794e5bf1891dd1d8f09fa3ce24e69
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-22 19:16:28 +0000

    Remove unused functions on indicator fetch service

--- gascd_frontend/src/services/indicator/IndicatorFetchService.ts
+++ gascd_frontend/src/services/indicator/IndicatorFetchService.ts
@@ -6,42 +6,6 @@ import { IndicatorQuery } from '@/data/interfaces/IndicatorQuery';
 import { TotalBedsFilters } from '@/data/interfaces/TotalBedsFilters';
 
 class IndicatorFetchService {
-  public static async getLocations(query: IndicatorQuery): Promise<Location[]> {
-    const response = await fetch('/api/get_location_names', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(query),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-
-    return response.json();
-  }
-
-  public static async getLocalAuthoritiesInProviderLocationRegion(
-    providerLocationId?: string
-  ): Promise<[]> {
-    const requestBody = providerLocationId
-      ? { provider_location_id: providerLocationId }
-      : {};
-    const response = await fetch('/api/get_locations', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-    return response.json();
-  }
-
   public static async getFilters(metric_id: string) {
     const response = await fetch('/api/get_all_total_beds_filters');
     if (!response.ok) {
@@ -65,33 +29,6 @@ class IndicatorFetchService {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(filteredQuery),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-    return response.json();
-  }
-
-  public static async getMetadateByType(query: string): Promise<MetaData[]> {
-    const response = await fetch(
-      `/api/get_metadata_by_data_types?metric_data_type=${query}`
-    );
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-    return await response.json();
-  }
-
-  public static async getDisplayData(
-    query: IndicatorQuery
-  ): Promise<IndicatorDisplay[]> {
-    const response = await fetch('/api/get_metadata', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(query),
     });
 
     if (!response.ok) {
