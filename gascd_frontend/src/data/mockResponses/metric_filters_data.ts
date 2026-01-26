051d5447124be0aa408453864d5357b4e97383eb
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-26 17:35:24 +0000

    Fix test data for filters

--- gascd_frontend/src/data/mockResponses/metric_filters_data.ts
+++ gascd_frontend/src/data/mockResponses/metric_filters_data.ts
@@ -3,35 +3,35 @@ export const metric_filters_data = {
   metric_filters: [
     {
       metric_id: 'bedcount_per_hundred_thousand_adults',
-      display_name: 'All bed types',
+      filter_type: 'All bed types',
     },
     {
       metric_group_id: 'bedcount_per_100000_adults',
       metric_id: 'bedcount_per_100000_adults_total_general_nursing',
-      display_name: 'General nursing',
+      filter_type: 'General nursing',
     },
     {
       metric_group_id: 'bedcount_per_100000_adults',
       metric_id:
         'bedcount_per_100000_adults_total_learning_disability_residential',
-      display_name: 'Learning disability residential',
+      filter_type: 'Learning disability residential',
     },
     {
       metric_group_id: 'bedcount_per_100000_adults',
       metric_id: 'bedcount_per_100000_adults_total_mental_health_residential',
-      display_name: 'Mental health residential',
+      filter_type: 'Mental health residential',
     },
     {
       metric_group_id: 'bedcount_per_100000_adults',
       metric_id:
         'bedcount_per_100000_adbedcount_per_100000_adults_total_transitionalults_total',
-      display_name: 'Transitional',
+      filter_type: 'Transitional',
     },
     {
       metric_group_id: 'bedcount_per_100000_adults',
       metric_id:
         'bedcount_per_100000_adults_total_ypd_young_physically_disabled',
-      display_name: 'Young physically disabled',
+      filter_type: 'Young physically disabled',
     },
   ],
 };
