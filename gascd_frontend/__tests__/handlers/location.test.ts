b01ccf12d31ee1d5a51fb1d44fcc1696f0f1e534
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-26 13:55:54 +0000

    Add a test for available locations

--- gascd_frontend/__tests__/handlers/location.test.ts
+++ gascd_frontend/__tests__/handlers/location.test.ts
@@ -1,6 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { locations_data } from '@/data/mockResponses/locations_data';
 import { GET as GetFilters } from '../../app/api/get_all_total_beds_filters/route';
+import { GET as GetAvailableLocations } from '../../app/api/get_available_locations/route';
+import { mockSession } from '@/test-utils/test-utils';
+import { auth } from '@/lib/auth';
+import { organisation_data } from '@/data/mockResponses/organisation_data';
+
+vi.mock('next/headers', () => ({
+  headers: vi.fn(),
+}));
+vi.mock('@/lib/auth', () => ({
+  auth: {
+    api: {
+      getSession: vi.fn(),
+    },
+  },
+}));
+const mockGetSession = vi.mocked(auth.api.getSession);
 
 vi.mock('server-only', () => ({
   default: vi.fn(),
@@ -54,5 +70,35 @@ describe('test handlers', () => {
       },
     ];
     expect(data).toEqual(expected_filters);
+  });
+});
+
+describe('test available locations', () => {
+  it('fetches throws error if no user', async () => {
+    const req = {
+      url: `http://localhost/api/get_available_locations`,
+    } as NextRequest;
+
+    const result = await GetAvailableLocations(req);
+    const data = await result.json();
+    expect(data).toEqual({ error: 'No user' });
+  });
+
+  it('fetches locations user', async () => {
+    mockGetSession.mockReturnValue(mockSession);
+
+    const req = {
+      url: `http://localhost/api/get_available_locations`,
+    } as NextRequest;
+
+    const result = await GetAvailableLocations(req);
+    const data = await result.json();
+    const location_listing = organisation_data.locations.map((item) => {
+      return {
+        location_name: item.location_name,
+        provider_name: organisation_data.display_name,
+      };
+    });
+    expect(data).toEqual({ data: location_listing });
   });
 });
