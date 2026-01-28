8f326b9f0fa8ac7bf33913db5d846321ff176f36
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-28 15:53:30 +0000

    Remove DB fallbacks in server routes

--- gascd_frontend/app/api/get_all_total_beds_filters/route.ts
+++ gascd_frontend/app/api/get_all_total_beds_filters/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { dbPool } from '../../../src/data/dbModule';
-import { TotalBedsFilters } from '@/data/interfaces/TotalBedsFilters';
 import logger from '@/utils/logger';
 import { getAPIClient } from '@/data/dataAPI';
 
@@ -32,28 +30,5 @@ export async function GET(req: NextRequest) {
         { status: 500 }
       );
     }
-  }
-
-  try {
-    const pool = await dbPool;
-    const resultSet = await pool
-      .request()
-      .query(
-        "SELECT DISTINCT metric_id, filter_bedtype FROM metrics.metadata WHERE group_id = 'bedcount_per_100000_adults'"
-      );
-    const rows: TotalBedsFilters[] = resultSet.recordset.map(
-      (row: TotalBedsFilters) => ({
-        ...row,
-        checked: false,
-      })
-    );
-    return NextResponse.json(rows);
-  } catch (err) {
-    logger.error('Error during database operations:', err);
-    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
-    return NextResponse.json(
-      { error: `Error fetching data: ${errorMessage}` },
-      { status: 500 }
-    );
   }
 }
--- gascd_frontend/app/api/get_available_locations/route.ts
+++ gascd_frontend/app/api/get_available_locations/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { dbPool } from '../../../src/data/dbModule';
-import logger from '@/utils/logger';
 import { getCurrentUser, isUserRegistered } from '@/lib/permissions';
 import { getAllowedLocations } from '@/data/locations';
 
@@ -21,27 +19,5 @@ export async function GET(req: NextRequest) {
         { status: 500 }
       );
     }
-  }
-
-  // XXX to remove
-  try {
-    const pool = await dbPool;
-    const resultSet = await pool
-      .request()
-      .input('provider_location_id', user.locationId)
-      .input('location_type', user.locationType).query(`
-        SELECT *
-        FROM access.metric_location_user_access
-        WHERE user_access_location_id = @provider_location_id AND user_access_location_type = @location_type AND metric_type = 'Capacity Tracker'
-      `);
-
-    return NextResponse.json(resultSet.recordset, { status: 200 });
-  } catch (err) {
-    logger.error('Error during database operations:', err);
-    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
-    return NextResponse.json(
-      { error: `Error fetching data: ${errorMessage}` },
-      { status: 500 }
-    );
   }
 }
