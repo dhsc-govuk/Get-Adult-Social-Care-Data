757daa56013a347f6334aef5c53165a629b84926
Robert Dunbar <robert.dunbar@madetech.com>
2026-02-05 11:32:00 +0000

    GASCDS-841: load version in constructor

--- gascd_api/api/Processors/VersionHandler.cs
+++ gascd_api/api/Processors/VersionHandler.cs
@@ -5,10 +5,25 @@ namespace api.Processors;
 
 public class VersionHandler : IGlobalPreProcessor
 {
+    private readonly string? _version;
+    private readonly ILogger<VersionHandler> _logger;
+
+    public VersionHandler(ILogger<VersionHandler> logger)
+    {
+        _version = GetInformationalVersion();
+        _logger = logger;
+    }
+
     public Task PreProcessAsync(IPreProcessorContext context, CancellationToken ct)
     {
-        var version = GetInformationalVersion();
-        context.HttpContext.Response.Headers.TryAdd("x-version", version);
+        try
+        {
+            context.HttpContext.Response.Headers.TryAdd("x-version", _version);
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning(e, "Failed to add version to http header response");
+        }
         return Task.CompletedTask;
     }
 
