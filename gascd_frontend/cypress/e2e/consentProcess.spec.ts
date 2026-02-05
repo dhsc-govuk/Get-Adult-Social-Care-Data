c506512259bcf11b66db84abfcadac4265d4c04f
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-02-05 16:11:00 +0000

    Add cypress test for marketing consent

---
+++ gascd_frontend/cypress/e2e/consentProcess.spec.ts
@@ -0,0 +1,27 @@
+describe('User can set consent', () => {
+  it('Should navigate to the consent page and set consent to true', () => {
+    cy.login();
+    cy.visit('/consent');
+
+    cy.get('h1').should(
+      'contains.text',
+      'Do you want to receive email updates'
+    );
+    cy.get('label').contains('Yes').click();
+    cy.get('button').contains('Continue').click();
+    cy.url().should('include', '/home');
+  });
+
+  it('Should navigate to the consent page and set consent to false', () => {
+    cy.login();
+    cy.visit('/consent');
+
+    cy.get('h1').should(
+      'contains.text',
+      'Do you want to receive email updates'
+    );
+    cy.get('label').contains('No').click();
+    cy.get('button').contains('Continue').click();
+    cy.url().should('include', '/home');
+  });
+});
