1117ac2b03c1e72568284daac1128a990e171a89
Katherine Hurst <katherine.hurst@madetech.com>
2026-02-10 15:04:36 +0000

    GASCDS-876: Add to context and extend metric time series class

--- gascd_api/api/Data/Models/Metrics/TimeSeries/ExpenditureLongtermSupportSetting.cs
+++ gascd_api/api/Data/Models/Metrics/TimeSeries/ExpenditureLongtermSupportSetting.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace api.Data.Models.Metrics.TimeSeries;
 
 [Table("expenditure_longterm_support_setting")]
-public class ExpenditureLongtermSupportSetting
+public class ExpenditureLongtermSupportSetting : MetricTimeSeries
 {
 
 }
