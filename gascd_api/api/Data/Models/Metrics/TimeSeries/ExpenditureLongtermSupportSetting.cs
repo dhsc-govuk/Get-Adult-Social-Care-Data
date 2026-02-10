358674f203d3da6393ff9e8d1a325bb9758c0677
Katherine Hurst <katherine.hurst@madetech.com>
2026-02-10 14:51:40 +0000

    GASCDS-876: Add first 3 tables

---
+++ gascd_api/api/Data/Models/Metrics/TimeSeries/ExpenditureLongtermSupportSetting.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Data.Models.Metrics.TimeSeries;
+
+[Table("expenditure_longterm_support_setting")]
+public class ExpenditureLongtermSupportSetting
+{
+
+}
---
+++ gascd_api/api/Data/Models/Metrics/TimeSeries/NumProviderLocations.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Data.Models.Metrics.TimeSeries;
+
+[Table("num_provider_locations")]
+public class NumProviderLocations : MetricTimeSeries
+{
+
+}
