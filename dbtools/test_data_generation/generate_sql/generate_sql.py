f3c1ac7434726a22d4b57fb24ababb1d1a89c0fe
Robert Dunbar <robert.dunbar@madetech.com>
2026-02-12 13:48:09 +0000

    GASCDS-886: update data, create metrics and groups if they dont exist

--- dbtools/test_data_generation/generate_sql/generate_sql.py
+++ dbtools/test_data_generation/generate_sql/generate_sql.py
@@ -12,13 +12,10 @@ def generate_sql(table_name, records):
         record_strings.append(record_string)
 
     values = ',\n'.join([f"({rs})" for rs in record_strings])
-
     return f"INSERT INTO {table_name} ({column_names_string})\n VALUES {values};"
-
 
 def format_date(date_obj):
     return date_obj.strftime('%Y-%m-%d')
-
 
 def generate_metric_record(table_name, record_idx, metric_code, location_code, location_type, start_date, end_date, time_series):
     return  {
@@ -33,10 +30,8 @@ def generate_metric_record(table_name, record_idx, metric_code, location_code, l
         "loaded_datetime": "CURRENT_TIMESTAMP"
     }
 
-
 def get_id_for_table_sql(table_name):
-    return f"with {table_name}_id as (select max(id) as id from {table_name})"
-
+    return f"with {table_name}_id as (select coalesce(max(id),0) as id from {table_name})"
 
 def get_id_by_code_sql(table, code):
     return f"(select id from {table} where code = '{code}')"
@@ -47,37 +42,28 @@ def get_id_field_sql(table, idx):
 def format_string(s):
     return f"'{s}'"
 
-
 def get_datapoint_count(start, end, frequency):
     delta = end - start
     if frequency == 'Daily':
-        return delta.days
+        return delta.days + 1
     elif frequency == 'Monthly':
-        return delta.days // 30
+        return (delta.days // 30) + 1
     elif frequency in ('Yearly', 'Financial yearly'):
-        return delta.days // 365
+        return (delta.days // 365) + 1
     elif frequency == 'Census':
         return 1
 
 def generate_deterministic_seed(metric_group, metric, location_id, salt="GASCD_metrics"):
-    """Generate a deterministic seed based on the combination of inputs"""
-
     combined_string = f"{salt}|{metric_group}|{metric}|{location_id}"
-
     hash_object = hashlib.sha256(combined_string.encode('utf-8'))
 
-    # Convert first 8 hex characters to integer
-    seed = int(hash_object.hexdigest()[:8], 16)
-    return seed
-
+    return int(hash_object.hexdigest()[:8], 16)
 
 def generate_time_series(metric_group, metric, location_code, count, data_range):
     min_val, max_val = data_range
-
     seed = generate_deterministic_seed(metric_group, metric, location_code)
 
     rng = random.Random(seed)
-
     if max_val.is_integer():
         return [rng.randint(int(min_val), int(max_val)) for _ in range(count)]
 
@@ -87,35 +73,69 @@ def generate_time_series(metric_group, metric, location_code, count, data_range)
 def generate_records_for_metric(metric_group, metric, data, idx):
     frequency = data['metric_date_type']
     location_types = data['location_types']
-
     start_date = DATE_RANGES[frequency]['start']
     end_date = DATE_RANGES[frequency]['end']
-
     datapoint_count = get_datapoint_count(start_date, end_date, frequency)
 
-
     records = []
-
     for location_type in location_types:
-
         for location_data in LOCATIONS[location_type]:
             time_series = generate_time_series(metric_group, metric, location_data['code'], datapoint_count, data['data_point_range'])
-
             record = generate_metric_record(metric_group, idx, metric, location_data['code'], location_type, start_date, end_date, time_series)
-
             records.append(record)
             idx += 1
 
     return records, idx
 
+def get_mg_record(mg):
+    return {
+        'id': get_max_id_sql('metric_groups'),
+        'code': format_string(mg),
+        'display_name': format_string(mg),
+        'loaded_datetime': 'CURRENT_TIMESTAMP'
+    }
+
+def get_max_id_sql(tn):
+    return f"(select max(id)+1 from {tn})"
+
+def get_m_record(mg, m):
+    return {
+        'id': get_max_id_sql('metrics'),
+        'code': format_string(m),
+        'metric_group_fk': get_id_by_code_sql('metric_groups', mg),
+        'filter_type': format_string('filter_type'),
+        'numerator_description': format_string('numerator_description'),
+        'denominator_description': format_string('demoninator_description'),
+        'data_source': format_string('ONS'),
+        'data_type': format_string('numbers'),
+        'frequency': format_string('Daily'),
+        'loaded_datetime': 'CURRENT_TIMESTAMP'
+    }
+
+def get_create_metric_group_if_not_exists_sql(mg):
+    mg_record = get_mg_record(mg)
+    insert_mg_sql = generate_sql('metric_groups', [mg_record])
+    return get_insert_if_not_exists_sql(get_id_by_code_sql('metric_groups', mg), insert_mg_sql)
+
+def get_insert_if_not_exists_sql(condition, insert_sql):
+    return f"do $$ begin if not exists {condition} then {insert_sql} end if; end $$;"
+
+def get_create_metric_if_not_exists_sql(mg, m):
+    m_record = get_m_record(mg, m)
+    insert_m_sql = generate_sql('metrics', [m_record])
+    return get_insert_if_not_exists_sql(get_id_by_code_sql('metrics', m), insert_m_sql)
 
 def generate_metric_sql():
     sqls = []
     for metric_group, metric_data in METRIC_DEFINITIONS.items():
+        mg_sql = get_create_metric_group_if_not_exists_sql(metric_group)
+        sqls.append(mg_sql)
         table_sql = get_id_for_table_sql(metric_group)
         idx = 1
 
         for metric, data in metric_data.items():
+            metric_sql = get_create_metric_if_not_exists_sql(metric_group, metric)
+            sqls.append(metric_sql)
             records, idx = generate_records_for_metric(metric_group, metric, data, idx)
             sqls.append(table_sql)
             sql = generate_sql(metric_group, records)
@@ -130,7 +150,6 @@ def generate_country_record(data, idx):
         'id': get_id_field_sql('countries', idx),
         'loaded_datetime': 'CURRENT_TIMESTAMP'
     }
-
 
 def generate_region_record(data, idx):
     return {
@@ -182,9 +201,7 @@ def generate_location_sql(table_name, location_type, generate_record_callable):
         idx += 1
 
     sql = generate_sql(table_name, records)
-
     return [table_sql, sql]
-
 
 def generate_reference_sql():
     country_sql = generate_location_sql('countries', 'National', generate_country_record)
@@ -195,11 +212,8 @@ def generate_reference_sql():
 
     return country_sql + region_sql + la_sql + cp_sql + cpl_sql
 
-
 def generate_all_sql():
-
     ref_sql = generate_reference_sql()
-
     metric_sql = generate_metric_sql()
 
     return ref_sql + metric_sql
