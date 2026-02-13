87d2f785f65a65197c3d52da9407155dc3ab98f5
Katherine Hurst <katherine.hurst@madetech.com>
2026-02-13 11:24:38 +0000

    GASCDS-872: reafactor into single query

--- gascd_api/api/Endpoints/Geo/CareProviderLocationNeighbours/GetCareProviderLocationNeighboursEndpoint.cs
+++ gascd_api/api/Endpoints/Geo/CareProviderLocationNeighbours/GetCareProviderLocationNeighboursEndpoint.cs
@@ -14,35 +14,40 @@ public class GetCareProviderLocationNeighboursEndpoint(GascdDataContext context,
 
     public override async Task HandleAsync(GetCareProviderLocationNeighboursRequest req, CancellationToken ct)
     {
-        var cpl = context.CareProviderLocations.Include(cpl => cpl.GeoData).SingleOrDefault(cpl => cpl.Code == req.CareProviderLocationCode);
-
-        if (cpl == null)
-        {
-            logger.LogInformation("Care provider location not found: {cpl}", req.CareProviderLocationCode);
-            await Send.NotFoundAsync(ct);
-            return;
-        }
-
-        if (cpl.GeoData == null)
-        {
-            logger.LogInformation("Care provider location: {cpl} has no GeoData", req.CareProviderLocationCode);
-            await Send.NotFoundAsync(ct);
-            return;
-        }
-
-        var cplCoord = cpl.GeoData.Coordinate;
-
         var distanceInDegrees = req.DistanceInKm / 111.139;
 
         var nearbyCpls = context.CareProviderLocations
-            .Include(l => l.LocalAuthority)
-            .Where(l => l.GeoData.Coordinate.IsWithinDistance(cplCoord, distanceInDegrees) && l.Code != req.CareProviderLocationCode)
+            .Include(cpl => cpl.LocalAuthority)
+            .Where(l => l.GeoData.Coordinate
+                .IsWithinDistance(context.CareProviderLocations
+                    .Where(l => l.Code == req.CareProviderLocationCode)
+                    .Select(targetCpl => targetCpl.GeoData.Coordinate)
+                    .FirstOrDefault(), distanceInDegrees)
+                        && l.Code != req.CareProviderLocationCode)
             .ToList();
+
+        if (nearbyCpls.Count == 0)
+        {
+            var cpl = context.CareProviderLocations.Include(cpl => cpl.GeoData).SingleOrDefault(cpl => cpl.Code == req.CareProviderLocationCode);
+            if (cpl == null)
+            {
+                logger.LogInformation("Care provider location not found: {cpl}", req.CareProviderLocationCode);
+                await Send.NotFoundAsync(ct);
+                return;
+            }
+            if (cpl.GeoData == null)
+            {
+                logger.LogInformation("Care provider location: {cpl} has no GeoData", req.CareProviderLocationCode);
+                await Send.NotFoundAsync(ct);
+                return;
+            }
+        }
 
         var neighbours = nearbyCpls.Select(mapper.CareProviderLocationToCareProviderLocationNeighbourResponse).ToList();
 
         var response = new GetCareProviderLocationNeighboursResponse { Locations = neighbours };
-        logger.LogInformation("Finished processing care provider location neighbours: {cpl}", req.CareProviderLocationCode);
+
         await Send.OkAsync(response, ct);
+
     }
 }
