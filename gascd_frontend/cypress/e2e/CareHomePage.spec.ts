67d42bd508d32a51878cfd4ccea1382938cae4e2
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-02-13 17:23:32 +0000

    Add some specific e2e tests for LA users

---
+++ gascd_frontend/cypress/e2e/CareHomePage.spec.ts
@@ -0,0 +1,182 @@
+describe('Care Home pages exist and contain data', () => {
+  it('CPL user should navigate to the Care home beds and occupancy levels page and check the contents', () => {
+    cy.login_onelogin(Cypress.env('cpl_user'));
+
+    cy.visit('/location-select');
+    cy.get('h1').should('contains.text', 'Select a location');
+    cy.get('label').contains('Test Care Provider Location 1').click();
+    cy.get('button').contains('Apply changes').click();
+
+    cy.visit('/topics/residential-care/provision-and-occupancy/data');
+
+    cy.get('h1').should('contains.text', 'Care home beds and occupancy levels');
+
+    cy.contains('Care home bed numbers');
+
+    cy.get('a[href*="chart-1"]').click();
+    cy.url().should('include', '#chart-1');
+    cy.get('#chart-1')
+      .should('be.visible')
+      .contains(
+        'Figure 1: chart of care home beds per 100,000 adult population'
+      );
+    cy.get('#table-1').should('not.be.visible');
+    cy.get('#download-1').should('not.be.visible');
+
+    cy.get('a[href*="table-1"]').click();
+    cy.url().should('include', '#table-1');
+    cy.get('#table-1')
+      .should('be.visible')
+      .contains(
+        'Table 1: care home bed numbers per 100,000 adult population for regional local authorities'
+      );
+    cy.get('#chart-1').should('not.be.visible');
+    cy.get('#download-1').should('not.be.visible');
+
+    cy.get('a[href*="download-1"]').click();
+    cy.url().should('include', '#download-1');
+    cy.get('#download-1').should('be.visible');
+    cy.get('#table-1').should('not.be.visible');
+    cy.get('#chart-1').should('not.be.visible');
+
+    cy.get('#numbers-table-metrics-button').click();
+    cy.get('#dhsc-filter--content1').should('be.visible');
+    cy.wait(300);
+    cy.get(
+      'input[type="radio"][value="bedcount_per_hundred_thousand_adults_total"]'
+    )
+      .check()
+      .should('be.checked');
+    cy.get('#numbers-table-metrics-submit-button').click();
+    cy.contains('Bed type: All bed types').should('be.visible');
+    cy.contains('Bed type: All bed types').click();
+    cy.contains('Bed type: All bed types').should('not.exist');
+
+    cy.contains('Care home bed types');
+
+    cy.get('a[href*="table-2"]').click();
+    cy.url().should('include', '#table-2');
+    cy.get('#table-2')
+      .should('be.visible')
+      .contains('Table 2: care home bed numbers per 100,000 adult population');
+    cy.get('#textSummary-2').should('not.be.visible');
+    cy.get('#download-2').should('not.be.visible');
+
+    cy.get('a[href*="textSummary-2"]').click();
+    cy.url().should('include', '#textSummary-2');
+    cy.get('#textSummary-2')
+      .should('be.visible')
+      .contains(
+        'The number of adult social care beds per 100,000 adult population provides an indicator of current care capacity'
+      );
+    cy.get('#table-2').should('not.be.visible');
+    cy.get('#download-2').should('not.be.visible');
+
+    cy.get('a[href*="download-2"]').click();
+    cy.url().should('include', '#download-2');
+    cy.get('#download-2').should('be.visible');
+    cy.get('#table-2').should('not.be.visible');
+    cy.get('#textSummary-2').should('not.be.visible');
+
+    cy.get('#type-table-metrics-button').click();
+    cy.get('#dhsc-filter--content1').should('be.visible');
+    cy.wait(300);
+    cy.get(
+      'input[type="checkbox"][value="bedcount_per_hundred_thousand_adults_total"]'
+    )
+      .check()
+      .should('be.checked');
+    cy.get('#type-table-metrics-submit-button').click();
+    cy.contains('Bed type: All bed types').should('be.visible');
+    cy.contains('Bed type: All bed types').click();
+    cy.contains('Bed type: All bed types').should('not.exist');
+
+    cy.contains('Beds per care home and occupancy levels');
+
+    cy.get('a[href*="table-3"]').click();
+    cy.url().should('include', '#table-3');
+    cy.get('#table-3')
+      .should('be.visible')
+      .contains('Table 3: care home bed numbers and occupancy levels');
+    cy.get('#textSummary-3').should('not.be.visible');
+    cy.get('#download-3').should('not.be.visible');
+
+    cy.get('a[href*="textSummary-3"]').click();
+    cy.url().should('include', '#textSummary-3');
+    cy.get('#textSummary-3')
+      .should('be.visible')
+      .contains('compared to the median');
+    cy.get('#download-3').should('not.be.visible');
+    cy.get('#table-3').should('not.be.visible');
+
+    cy.get('a[href*="download-3"]').click();
+    cy.url().should('include', '#download-3');
+    cy.get('#download-3').should('be.visible');
+    cy.wait(500);
+    cy.get('#textSummary-3').should('not.be.visible');
+    cy.get('#table-3').should('not.be.visible');
+
+    cy.contains('Care home bed numbers - trends over time');
+
+    cy.get('a[href*="graph-4"]').click();
+    cy.url().should('include', '#graph-4');
+    cy.get('#graph-4')
+      .should('be.visible')
+      .contains(
+        'Figure 2: graph of care home bed numbers per 100,000 adult population'
+      );
+    cy.get('#table-3').should('not.be.visible');
+    cy.get('#textSummary-3').should('not.be.visible');
+
+    cy.get('#type-chart-metrics-button').click();
+    cy.get('#dhsc-filter--content1').should('be.visible');
+    cy.wait(300);
+    cy.get(
+      'input[type="checkbox"][value="bedcount_per_hundred_thousand_adults_total"]'
+    )
+      .check()
+      .should('be.checked');
+    cy.get('#type-chart-metrics-submit-button').click();
+    cy.contains('Bed type: All bed types').should('be.visible');
+    cy.contains('Bed type: All bed types').click();
+    cy.contains('Bed type: All bed types').should('not.exist');
+  });
+
+  it('LA user should navigate to the Care home beds and occupancy levels page and check the contents', () => {
+    cy.login_onelogin(Cypress.env('la_user'));
+
+    cy.visit('/topics/residential-care/provision-and-occupancy/data');
+
+    cy.get('h1').should('contains.text', 'Care home beds and occupancy levels');
+
+    cy.contains('Care home bed numbers');
+
+    cy.get('a[href*="chart-1"]').click();
+    cy.url().should('include', '#chart-1');
+    cy.get('#chart-1')
+      .should('be.visible')
+      .contains(
+        'Figure 1: chart of care home beds per 100,000 adult population'
+      );
+    cy.get('#table-1').should('not.be.visible');
+    cy.get('#download-1').should('not.be.visible');
+
+    // CP metrics not visible to LAs
+    cy.get('a[href*="table-3"]').click();
+    cy.url().should('include', '#table-3');
+    cy.get('#table-3')
+      .should('be.visible')
+      .contains('Table 3: care home bed numbers and occupancy levels');
+    cy.get('#textSummary-3').should('not.be.visible');
+    cy.get('#download-3').should('not.be.visible');
+
+    cy.get('a[href*="textSummary-3"]').click();
+    cy.url().should('include', '#textSummary-3');
+    cy.get('#textSummary-3')
+      .should('be.visible')
+      .should('not.contains.text', 'compared to the median')
+      .should('contains.text', 'than the regional average');
+    cy.get('#download-3').should('not.be.visible');
+    cy.get('#table-3').should('not.be.visible');
+  });
+});
--- gascd_frontend/cypress/e2e/locationSelectProcess.spec.ts
+++ gascd_frontend/cypress/e2e/locationSelectProcess.spec.ts
@@ -1,6 +1,31 @@
 describe('User can set location', () => {
-  it('Should navigate to the location select page and change the location', () => {
+  it('CPL user should navigate to the location select page and change the location', () => {
     cy.login_onelogin(Cypress.env('cpl_user'));
+
+    // test cpl user already has location set up
+    cy.get('#navigation .govuk-service-navigation__link-change')
+      .should('contains.text', 'Change')
+      .click();
+    cy.url().should('include', '/location-select');
+
+    // Verify that we are on the location select page
+    cy.get('h1').should('contains.text', 'Select a location');
+    cy.get('label').contains('Test Care Provider Location 1').click();
+    cy.get('button').contains('Apply changes').click();
+
+    cy.url().should('include', '/home');
+
+    // Location has been updated
+    cy.get('#navigation').should(
+      'contains.text',
+      'Test Care Provider Location'
+    );
+  });
+
+  it('CP user should navigate to the location select page and change the location', () => {
+    cy.login_onelogin(Cypress.env('cp_user'));
+
+    // test cp user does not have location yet
     cy.visit('/location-select');
 
     // Verify that we are on the location select page
@@ -9,5 +34,25 @@ describe('User can set location', () => {
     cy.get('button').contains('Apply changes').click();
 
     cy.url().should('include', '/home');
+
+    // Location has been updated
+    cy.get('#navigation').should(
+      'contains.text',
+      'Test Care Provider Location'
+    );
+    cy.get('#navigation .govuk-service-navigation__link-change')
+      .should('contains.text', 'Change')
+      .click();
+    cy.url().should('include', '/location-select');
+  });
+
+  it('LA user should have location set already', () => {
+    cy.login_onelogin(Cypress.env('la_user'));
+    cy.url().should('include', '/home');
+
+    cy.get('#navigation .govuk-service-navigation__link-change').should(
+      'not.exist'
+    );
+    cy.get('[data-testid="selected-location"]').should('exist');
   });
 });
