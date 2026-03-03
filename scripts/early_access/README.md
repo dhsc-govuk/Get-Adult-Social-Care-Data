a1dbab9122b3777f250ac6b370c8c5cc6a0781b1
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-03-03 19:16:15 +0000

    Clean up CQC matching scripts and add duckdb SQL processing

---
+++ scripts/early_access/README.md
@@ -0,0 +1,8 @@
+# Early access list processing scripts
+
+Usage:
+
+./scripts/early_access/read_contacts_file.py /path/to/cqc-contacts.xlsx
+./scripts/early_access/read_ni_list.py /path/to/waitlist.xlsx
+./scripts/early_access/join_contacts_ni_lists.py
+
---
+++ scripts/early_access/join_contacts_ni_lists.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "duckdb",
+# ]
+# ///
+# Join CQC and waitlist files using duckdb, grouping 
+
+import duckdb
+import os
+
+def join_data(outfilename):
+    cwd = os.path.dirname(__file__)
+    SQL = f"""
+    with contacts as (
+        select
+            email,
+            string_agg(location_id) as locations
+        from
+            '{cwd}/cleaned_data/contacts.parquet'
+        where
+            location_type = 'Provider'
+            and role = 'Nominated Individual'
+        group by email
+    ),
+
+    nis as (
+        select distinct
+            email as ni_email,
+            name,
+            is_ni,
+            list_consent
+        from
+            '{cwd}/cleaned_data/ni-list.parquet'
+    )
+
+    select
+        nis.name,
+        nis.ni_email,
+        contacts.locations,
+        'Care provider' as location_type,
+        'CQC-NI-LIST' as source
+    from nis
+    inner join contacts on nis.ni_email = contacts.email
+    -- where email not in (select email from existing)
+    """
+
+    CSV_SQL = f"""COPY ({SQL}) TO '{outfilename}' (HEADER, DELIMITER ',')"""
+    duckdb.sql(CSV_SQL)
+
+if __name__ == "__main__":
+    cwd = os.path.dirname(__file__)
+    outfilename = os.path.join(cwd, "cleaned_data/joined.csv")
+    join_data(outfilename)
+    print("Data written to", outfilename)
