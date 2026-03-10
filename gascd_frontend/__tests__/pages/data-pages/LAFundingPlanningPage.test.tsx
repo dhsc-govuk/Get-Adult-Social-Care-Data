ea91ae476cce8318c026b36a9468cbb748cbc921
Rachel Dodson <rachel.dodson@madetech.com>
2026-03-10 15:11:02 +0000

    [1044] Add to test

--- gascd_frontend/__tests__/pages/data-pages/LAFundingPlanningPage.test.tsx
+++ gascd_frontend/__tests__/pages/data-pages/LAFundingPlanningPage.test.tsx
@@ -55,10 +55,13 @@ describe('LAFundingPage', () => {
     expect(bodyTextElement).toBeInTheDocument();
 
     const helpLink = screen.getAllByRole('link', {
-      name: /local authority funding planning/i,
+      name: /Total population aged 30-64 to have early onset dementia/i,
     });
     expect(helpLink[0]).toBeInTheDocument();
-    expect(helpLink[0]).toHaveAttribute('href', '/help/la-funding-planning');
+    expect(helpLink[0]).toHaveAttribute(
+      'href',
+      '/help/estimated-population-early-onset-dementia'
+    );
 
     const tableElement = screen.getByRole('table');
     expect(tableElement).toBeInTheDocument();
