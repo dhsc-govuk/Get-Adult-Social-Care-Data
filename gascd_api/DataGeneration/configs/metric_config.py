c3706aa81d62ecdf0434ddd8a8b2f4452cb8d395
Robert Dunbar <robert.dunbar@madetech.com>
2026-03-11 16:20:45 +0000

    GASCDS-1056: update readme, delete old test data generation scripts

--- gascd_api/DataGeneration/configs/metric_config.py
+++ gascd_api/DataGeneration/configs/metric_config.py
@@ -80,22 +80,18 @@ DATE_RANGES = {
     'Daily': {
         'start': date(2024, 1, 1),
         'end': date(2024, 2, 1),
-        'format': 'YYYY-MM-DD'
     },
     'Monthly': {
         'start': date(2023, 1, 1),
         'end': date(2024, 12, 1),
-        'format': 'YYYY-MM-01'
     },
     'Yearly': {
         'start': date(2020, 1, 1),
         'end': date(2025, 1, 1),
-        'format': 'YYYY-01-01'
     },
     'Census': {
         'start': date(2021, 1, 1),
         'end': date(2021, 1, 1),
-        'format': 'YYYY-01-01'
     },
     'Financial yearly': {
         'start': date(2021,4,1),
