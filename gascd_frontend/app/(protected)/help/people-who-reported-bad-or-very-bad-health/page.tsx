78c20e98b5fa78e91e5bd8ef3991a61db8931bde
Katherine Hurst <katherine.hurst@madetech.com>
2026-03-13 13:54:59 +0000

    GASCDS-1065: bad health page update

--- gascd_frontend/app/(protected)/help/people-who-reported-bad-or-very-bad-health/page.tsx
+++ gascd_frontend/app/(protected)/help/people-who-reported-bad-or-very-bad-health/page.tsx
@@ -40,9 +40,6 @@ const BadHealth = () => {
           limitations={
             <p className="govuk-!-margin-top-0">See data source for details.</p>
           }
-          dataDefinitions={
-            <p className="govuk-!-margin-top-0">See data source for details.</p>
-          }
         />
       </Layout>
     </>
