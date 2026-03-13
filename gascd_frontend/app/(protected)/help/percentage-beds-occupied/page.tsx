a9ed3a96e0978492eca0cf40493c1e113ee298ea
Katherine Hurst <katherine.hurst@madetech.com>
2026-03-13 13:59:55 +0000

    GASCDS-1065: percentage beds occupied page update

--- gascd_frontend/app/(protected)/help/percentage-beds-occupied/page.tsx
+++ gascd_frontend/app/(protected)/help/percentage-beds-occupied/page.tsx
@@ -33,73 +33,76 @@ const PercentageBedsOccupied: React.FC = () => {
           updateFrequency="Daily"
           methodology={
             <>
-              <p className="govuk-!-margin-top-0">
+              <p className="govuk-body">
                 The occupancy rate is calculated by dividing the number of
-                occupied beds by the total number of beds. &lsquo;Beds&rsquo;
-                refers to adult social care beds in care providers and includes
-                the following categories:
+                occupied beds by the total number of beds. ‘Beds’ refers to
+                adult social care beds in care providers and includes the
+                following categories:
               </p>
               <ul className="govuk-list govuk-list--bullet">
-                <li>general residential</li>
-                <li>general nursing</li>
+                <li>community care</li>
                 <li>dementia residential</li>
                 <li>dementia nursing</li>
-                <li>mental health residential</li>
-                <li>mental health nursing</li>
-                <li>learning disability residential</li>
+                <li>general nursing</li>
+                <li>general residential</li>
                 <li>learning disability nursing</li>
+                <li>learning disability residential</li>
+                <li>mental health nursing</li>
+                <li>mental health residential</li>
                 <li>transitional care</li>
                 <li>young physically disabled</li>
               </ul>
-              <p>
-                &lsquo;Occupied beds&rsquo; refers to beds reported as being in
-                use at the time of data collection.
+              <p className="govuk-body">
+                ‘Occupied beds’ refers to beds reported as being in use at the
+                time of data collection.
               </p>
-              <p>
-                Care providers registered with the Care Quality Commission (CQC)
-                must update this information at least monthly using the Capacity
-                Tracker tool. The mandated reporting period is between the 8th
-                and 14th every month, or the next working day if the 14th falls
-                on a weekend or holiday.
+              <p className="govuk-body">
+                Care providers registered with the{' '}
+                <abbr title="Care Quality Commission">CQC</abbr> must update
+                this information at least monthly using the Capacity Tracker
+                tool. The mandated reporting period is between the 8th and 14th
+                every month, or the next working day if the 14th falls on a
+                weekend or holiday.
               </p>
-              <p>
-                Small number suppression is applied. Occupied bed counts less
-                than 5 and their corresponding percentages have been suppressed.
+              <p className="govuk-body">
+                Bed counts and occupied bed counts are suppressed at provider
+                location level where they fall below 6, to protect the
+                confidentiality of individuals, along with their corresponding
+                percentages. At local authority, regional, and national level,
+                figures are also suppressed where there are too few care
+                providers contributing to a total, to avoid identification of
+                individual provider figures by subtraction. Suppressed values
+                appear as 0. The Isles of Scilly are excluded at all geographic
+                levels.
               </p>
             </>
           }
           limitations={
             <>
-              <p className="govuk-!-margin-top-0">
+              <p className="govuk-body">
                 Care providers may update their Capacity Tracker data at
                 different times outside the reporting period.
               </p>
-              <p>
+              <p className="govuk-body">
                 As a result, the data does not provide a snapshot of all
                 providers at the same time. It reflects the most recent
                 information available when the data was retrieved.
               </p>
-              <p>
-                The data is self&#45;reported and not independently verified.
+              <p className="govuk-body">
+                The data is self-reported and not independently verified.
               </p>
-              <p>
+              <p className="govuk-body">
                 When adult social care beds are vacant, they can be used
                 flexibly across a range of bed types. We are exploring ways to
                 reflect this in the data.
               </p>
-              <p>
+              <p className="govuk-body">
                 The current bed types are not clearly defined and may be
                 interpreted differently by care providers submitting data. To
                 improve consistency, we are working with Capacity Tracker to
                 explore whether more detailed bed descriptions can be provided.
               </p>
             </>
-          }
-          dataDefinitions={
-            <p className="govuk-!-margin-top-0">
-              Refer to the Limitations section for details about work relating
-              to bed type descriptions.
-            </p>
           }
         />
       </Layout>
