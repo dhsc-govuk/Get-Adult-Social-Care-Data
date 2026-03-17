2faaed21544f55335f45f55285733f64d77794a8
Robert Dunbar <robert.dunbar@madetech.com>
2026-03-17 16:00:32 +0000

    GASCDS-1081: open xlsx file

--- ni_api/core.Tests/ExcelReader/ExcelReaderTests.cs
+++ ni_api/core.Tests/ExcelReader/ExcelReaderTests.cs
@@ -1,14 +1,40 @@
+using Shouldly;
+using System;
+using System.IO;
 using Xunit;
 
 namespace core.Tests.ExcelReader;
 
-public class ExcelReaderTests
+public class ExcelReaderTests : IDisposable
 {
+    FileStream _fileStream;
 
     [Fact]
     public void CanCreateExcelReader()
     {
-        Reader.ExcelReader excelReader = new Reader.ExcelReader();
+        Reader.ExcelReader excelReader = new Reader.ExcelReader(GetTestFileStream());
         Assert.NotNull(excelReader);
+    }
+
+    [Fact]
+    public void CanReadExcelFile()
+    {
+        Reader.ExcelReader excelReader = new Reader.ExcelReader(GetTestFileStream());
+        int? sheetCount = excelReader.GetSheetCount();
+        sheetCount.ShouldBe(3);
+    }
+
+    private FileStream GetTestFileStream()
+    {
+        _fileStream = File.Open("TestData/test_ni_list.xlsx", FileMode.Open, FileAccess.Read);
+        return _fileStream;
+    }
+
+    public void Dispose()
+    {
+        if (_fileStream != null)
+        {
+            _fileStream.Dispose();
+        }
     }
 }
--- ni_api/core/Reader/ExcelReader.cs
+++ ni_api/core/Reader/ExcelReader.cs
@@ -1,6 +1,18 @@
+using DocumentFormat.OpenXml.Packaging;
+
 namespace core.Reader;
 
 public class ExcelReader
 {
+    private SpreadsheetDocument _document;
     
+    public ExcelReader(FileStream stream)
+    {
+        _document = SpreadsheetDocument.Open(stream, false);
+    }
+
+    public int? GetSheetCount()
+    {
+        return _document.WorkbookPart?.WorksheetParts?.Count();
+    }
 }
