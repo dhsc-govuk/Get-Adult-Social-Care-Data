140a2225d9b2032b33dce54555cdeb226a311e78
Rachel Dodson <rachel.dodson@madetech.com>
2026-03-19 14:44:31 +0000

    [1086] remove console.log

--- gascd_frontend/app/(protected)/topics/(protected-la-metrics)/future-planning/la-funding-planning/data/page.tsx
+++ gascd_frontend/app/(protected)/topics/(protected-la-metrics)/future-planning/la-funding-planning/data/page.tsx
@@ -146,7 +146,6 @@ export default function LAFundingPlanningPage() {
       }
     };
     fetchAllData();
-    console.log('filteredDemographicData', filteredDemographicData);
   }, [demographicQuery]);
 
   useEffect(() => {
