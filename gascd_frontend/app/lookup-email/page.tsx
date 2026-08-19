07b4b03f7b64f25f43e0bbe140c4703c87570720
Chie <cichongiri@deloitte.co.uk>
2026-08-19 15:25:32 +0100

    wip: Define Server Action to receive form-data for email processing

--- gascd_frontend/app/lookup-email/page.tsx
+++ gascd_frontend/app/lookup-email/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import Form from 'next/form';
 import Layout from '@/components/common/layout/Layout';
 import LookupLA from '@/components/email-lookup/LookupLA';
 import { redirect } from 'next/navigation';
+import { checkEmailDomain } from './actions';
+import { isBadQueryStringValue } from '@/lib/domain-check';
 
-const EXPECTED_ID_VALUES = ['u:ni', 'u:rm', 'u:la', 'u:x'];
 type Props = {
   searchParams: Promise<{ id?: string }>;
 };
 const EmailLookupPage: React.FC<Props> = async ({ searchParams }) => {
   const { id } = await searchParams;
-  if (
-    id == null ||
-    EXPECTED_ID_VALUES.includes(id) === false ||
-    id !== 'u:la'
-  ) {
+
+  if (isBadQueryStringValue(id)) {
     redirect('/login');
   }
 
@@ -27,7 +27,55 @@ const EmailLookupPage: React.FC<Props> = async ({ searchParams }) => {
         currentPage="lookup-email"
         showNavBar={false}
       >
-        <LookupLA />
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <div className="govuk-form-group">
+              <Form action={checkEmailDomain}>
+                <LookupLA />
+
+                {/* Form Trigger(s) */}
+                <div className="govuk-button-group govuk-!-margin-top-6">
+                  <button
+                    type="submit"
+                    className="govuk-button"
+                    data-module="govuk-button"
+                  >
+                    Continue
+                  </button>
+                  {/* <button
+                type="submit"
+                className="govuk-button govuk-button--start"
+                data-module="govuk-button"
+                disabled={isPending}
+              >
+                {isPending ? 'Signing in...' : 'Continue'}
+                {!isPending && (
+                  <svg
+                    className="govuk-button__start-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="17.5"
+                    height="19"
+                    viewBox="0 0 33 40"
+                    aria-hidden="true"
+                    focusable="false"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M0 0h13l20 20-20 20H0l20-20z"
+                    />
+                  </svg>
+                )}
+              </button>
+              {state && <p className="govuk-error-message">{state.error}</p>} */}
+
+                  <Link href="#" className="govuk-link">
+                    Cancel and go back
+                  </Link>
+                </div>
+              </Form>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
