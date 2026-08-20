65567c86761883d6b117ec978a96f778b1ed481f
Chie <cichongiri@deloitte.co.uk>
2026-08-20 09:28:13 +0100

    wip: Update hint text for email-address field

--- gascd_frontend/src/components/email-lookup/LookupLA.tsx
+++ gascd_frontend/src/components/email-lookup/LookupLA.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const LookupLA: React.FC = () => {
-  const showDomainNoMatchWarning = true;
   return (
     <fieldset className="govuk-fieldset" aria-describedby="signIn-hint">
       <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
@@ -10,12 +9,11 @@ const LookupLA: React.FC = () => {
 
       <div className="govuk-form-group">
         <label className="govuk-label" htmlFor="la-user-email">
-          Email
+          Email address
         </label>
-        {/* <div id="la-user-email-hint" className="govuk-hint">
-                  A domain match must be found before Location ID can be
-                  selected.
-                </div> */}
+        <p id="la-user-email-hint" className="govuk-hint govuk-!-margin-top-0">
+          It will be checked against a list of approved LA domains.
+        </p>
         <input
           id="la-user-email"
           className="govuk-input govuk-!-width-one-third"
@@ -26,17 +24,6 @@ const LookupLA: React.FC = () => {
           // onChange={(event) => setEmail(event.target.value)}
           // disabled={submitting}
         />
-        {showDomainNoMatchWarning ? (
-          <p className="govuk-warning-text govuk-!-margin-top-2 govuk-!-margin-bottom-0">
-            <span className="govuk-warning-text__icon" aria-hidden="true">
-              !
-            </span>
-            <strong className="govuk-warning-text__text">
-              <span className="govuk-visually-hidden">Warning</span>
-              No matching council domain was found for this email address.
-            </strong>
-          </p>
-        ) : null}
       </div>
     </fieldset>
   );
