d282205cfbf6e656079418a3ed77609139523ff6
Chie <cichongiri@deloitte.co.uk>
2026-08-23 20:57:09 +0100

    wip: Define form schema for SignupLA page

--- gascd_frontend/src/server-actions/index.ts
+++ gascd_frontend/src/server-actions/index.ts
@@ -2,10 +2,35 @@
 import logger from '@/utils/logger';
 import { isAcceptableEmail } from '@/lib/domain-check';
 import { redirect } from 'next/navigation';
-import type { ActionResponse, WhoamiErrors, WhoamiFormData } from './types';
+import type { ActionResponse, SignupLAData, WhoamiFormData } from './types';
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
 
+export async function handleFormSignupLA(
+  prev: ActionResponse<SignupLAData> | null,
+  formData: FormData
+): Promise<ActionResponse<SignupLAData> | undefined> {
+  const regfullname = formData.get('regfullname');
+  const regla = formData.get('regla');
+  const regmail = formData.get('regmail');
+  const regorgname = formData.get('regorgname');
+  const regrole = formData.get('regrole');
+
+  const rawFormData: SignupLAData = {
+    regfullname: isNonEmptyString(regfullname) ? regfullname : null,
+    regla: isNonEmptyString(regla) ? regla : null,
+    regmail: isNonEmptyString(regmail) ? regmail : null,
+    regorgname: isNonEmptyString(regorgname) ? regorgname : null, // Optional
+    regrole: isNonEmptyString(regrole) ? regrole : null,
+  };
+
+  // ...
+  return;
+}
+
+function isNonEmptyString(value: unknown): value is string {
+  return typeof value === 'string' && value.trim().length > 0;
+}
 export async function checkEmailDomain(formData: FormData) {
   const rawFormData = {
     email: formData.get('email'),
@@ -22,15 +47,12 @@ export async function checkEmailDomain(formData: FormData) {
 }
 
 export async function processWhoami(
-  previousState: ActionResponse | null,
+  previousState: ActionResponse<WhoamiFormData> | null,
   formData: FormData
-): Promise<ActionResponse | undefined> {
-  // const { data, error } = await authClient.signIn.oauth2({
-  //   providerId: 'govuk-one-login',
-  //   callbackURL: `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/home`,
-  // });
+): Promise<ActionResponse<WhoamiFormData> | undefined> {
+  const _id = formData.get('id');
   const rawFormData: WhoamiFormData = {
-    id: formData.get('id') as string,
+    id: isNonEmptyString(_id) ? _id : null,
   };
 
   const errors = validateFormFields(rawFormData);
@@ -112,11 +134,12 @@ export async function processWhoami(
   // };
 }
 
+type WhoamiErrors = Partial<WhoamiFormData>;
 function validateFormFields(fields: WhoamiFormData): WhoamiErrors {
   const errors: WhoamiErrors = {};
 
   const EXPECTED_ID_VALUES = ['u:la', 'u:cqc', 'u:x'];
-  if (EXPECTED_ID_VALUES.includes(fields.id) === false) {
+  if (fields.id == null || EXPECTED_ID_VALUES.includes(fields.id) === false) {
     errors.id = 'Select an option';
   }
 
