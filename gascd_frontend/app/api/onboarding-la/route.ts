b41aa6c869e0f43ecfe815c7c6a70052839c220d
Chie <cichongiri@deloitte.co.uk>
2026-08-31 18:52:48 +0100

    wip: Create new API route to handle CreateNewUser operation

---
+++ gascd_frontend/app/api/onboarding-la/route.ts
@@ -0,0 +1,11 @@
+import { createNewDBUser } from '@/lib/create-new-user';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  const submittedData = await req.json();
+
+  const result = await createNewDBUser(submittedData.email ?? null);
+  console.log('$$$', req.url, submittedData, result);
+
+  return NextResponse.json({ result: 'XYZ' }, { status: 200 });
+}
