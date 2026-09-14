a981f9261ddf79947e7c9b47f0d0c1442cd77c08
Chie <308352618+chiich-deloitte@users.noreply.github.com>
2026-09-14 17:03:40 +0100

    wip: Revert changes made during attempts to use Server Actions, relating to Better AUth

--- gascd_frontend/src/lib/basePath.ts
+++ gascd_frontend/src/lib/basePath.ts
@@ -1,8 +1,2 @@
-export const withBasePath = (path: string, context?: 'client' | 'server') => {
-  const env =
-    context === 'server'
-      ? process.env.BASE_PATH
-      : process.env.NEXT_PUBLIC_BASE_PATH;
-
-  return `${env ?? ''}${path}`;
-};
+export const withBasePath = (path: string) =>
+  `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}${path}`;
